8dfc1c8d35be3b68abe10618ee620ffa3d3516ba
YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr <YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr>
2012-03-21 14:12:42 +0100

    TestMP3

---
+++ TestMP3/TestMP3/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TestMP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.play = new System.Windows.Forms.Button();
+            this.pause = new System.Windows.Forms.Button();
+            this.browse = new System.Windows.Forms.Button();
+            this.filenameSong = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // play
+            // 
+            this.play.Location = new System.Drawing.Point(12, 155);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(75, 23);
+            this.play.TabIndex = 0;
+            this.play.Text = "Play";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            // 
+            // pause
+            // 
+            this.pause.Enabled = false;
+            this.pause.Location = new System.Drawing.Point(205, 155);
+            this.pause.Name = "pause";
+            this.pause.Size = new System.Drawing.Size(75, 23);
+            this.pause.TabIndex = 1;
+            this.pause.Text = "Pause";
+            this.pause.UseVisualStyleBackColor = true;
+            this.pause.Click += new System.EventHandler(this.pause_Click);
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(205, 88);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(75, 23);
+            this.browse.TabIndex = 2;
+            this.browse.Text = "Browse";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // filenameSong
+            // 
+            this.filenameSong.Location = new System.Drawing.Point(12, 88);
+            this.filenameSong.Name = "filenameSong";
+            this.filenameSong.Size = new System.Drawing.Size(187, 20);
+            this.filenameSong.TabIndex = 3;
+            this.filenameSong.Text = "D:\\Data\\Music\\Jango\\Celldweller - Switchback.mp3";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Controls.Add(this.filenameSong);
+            this.Controls.Add(this.browse);
+            this.Controls.Add(this.pause);
+            this.Controls.Add(this.play);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.Button pause;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.TextBox filenameSong;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ TestMP3/TestMP3/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+using System.Threading;
+
+
+//http://forum.codecall.net/csharp-tutorials/20420-tutorial-playing-mp3-files-c.html#post199646
+
+namespace TestMP3
+{
+    public partial class Form1 : Form
+    {
+        bool playing = false;
+        bool paused = false;
+
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void play_Click(object sender, EventArgs e)
+        {
+            if (!playing)
+            {
+                long ret = mciSendString("open \"" + filenameSong.Text /*"D:/Data/Music/Jango/Celldweller - Switchback.mp3"*/ + "\" type mpegvideo alias MediaFile", null, 0, IntPtr.Zero);
+                ret = mciSendString("play MediaFile", null, 0, IntPtr.Zero);
+
+                playing = true;
+                play.Text = "Stop";
+                pause.Enabled = true;
+            }
+            else
+            {
+                mciSendString("close MediaFile", null, 0, IntPtr.Zero);
+
+                playing = false;
+                play.Text = "Play";
+                pause.Enabled = false;
+            }
+        }
+
+        private void pause_Click(object sender, EventArgs e)
+        {
+            if (!paused)
+            {
+                mciSendString("pause MediaFile", null, 0, IntPtr.Zero);
+
+                paused = true;
+                pause.Text = "Resume";
+            }
+            else
+            {
+                mciSendString("resume MediaFile", null, 0, IntPtr.Zero);
+
+                paused = false;
+                pause.Text = "Pause";
+            }
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filenameSong.Text = openFileDialog1.FileName;                
+            }
+        }
+    }
+}
