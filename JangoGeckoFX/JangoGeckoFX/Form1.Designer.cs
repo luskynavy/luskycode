68f4401c57e764451bb775fca5335da4d0021b8c
YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr <YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr>
2012-03-22 12:33:08 +0100

    JangoGeckoFX 11-08-21 solution creation all ok

---
+++ JangoGeckoFX/JangoGeckoFX/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace JangoGeckoFX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ReHook = new System.Windows.Forms.Button();
+            this.toolTipReHook = new System.Windows.Forms.ToolTip(this.components);
+            this.update = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // ReHook
+            // 
+            this.ReHook.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ReHook.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.ReHook.Location = new System.Drawing.Point(0, 0);
+            this.ReHook.Name = "ReHook";
+            this.ReHook.Size = new System.Drawing.Size(23, 23);
+            this.ReHook.TabIndex = 0;
+            this.ReHook.Text = "R";
+            this.toolTipReHook.SetToolTip(this.ReHook, "Reinstall hook keys");
+            this.ReHook.UseVisualStyleBackColor = false;
+            this.ReHook.Click += new System.EventHandler(this.ReHook_Click);
+            // 
+            // update
+            // 
+            this.update.Enabled = true;
+            this.update.Interval = 1000;
+            this.update.Tick += new System.EventHandler(this.update_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 321);
+            this.Controls.Add(this.ReHook);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ReHook;
+        private System.Windows.Forms.ToolTip toolTipReHook;
+        private System.Windows.Forms.Timer update;
+    }
+}
+
---
+++ JangoGeckoFX/JangoGeckoFX/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Skybound.Gecko;
+
+// Need add references to C:\Users\Tigra\Downloads\Skybound.GeckoFX.bin.v1.9.1.0\bin\Skybound.Gecko.dll
+// from http://code.google.com/p/geckofx/downloads/list Skybound.GeckoFX.bin.v1.9.1.0.zip
+
+// works with ftp://ftp.mozilla.org/pub/xulrunner/releases/1.9.0.13/runtimes/xulrunner-1.9.0.13.en-US.win32.zip
+// FAIL 6.0, 5.0, 2.0
+
+namespace JangoGeckoFX
+{
+    public partial class Form1 : Form
+    {
+        public int volume = 50;
+        public int lastVolumeChange = 0;
+        public bool noTitle = false;
+        public Hooks hook;
+        GeckoWebBrowser webBrowser1;
+
+        public Form1()
+        {
+            //Skybound.Gecko.Xpcom.Initialize(@"C:\Users\Tigra\Downloads\xulrunner-1.9.0.13.en-US.win32\xulrunner"); //OK
+            Skybound.Gecko.Xpcom.Initialize(@"C:\Users\Tigra\Downloads\xulrunner-1.9.2.19.en-US.win32\xulrunner"); //OK
+            //Skybound.Gecko.Xpcom.Initialize(@"C:\Users\Tigra\Downloads\xulrunner-6.0.en-US.win32\xulrunner"); // FAIL: Le cast spécifié n'est pas valide.
+            //Skybound.Gecko.Xpcom.Initialize(@"C:\Users\Tigra\Downloads\xulrunner-5.0.en-US.win32\xulrunner"); // FAIL: Le cast spécifié n'est pas valide.
+            //Skybound.Gecko.Xpcom.Initialize(@"C:\Users\Tigra\Downloads\xulrunner-2.0.en-US.win32\xulrunner"); // FAIL: Le cast spécifié n'est pas valide.
+            //Skybound.Gecko.Xpcom.Initialize(@"C:\NVN\FirefoxPortable 4\App\Firefox"); // FAIL: Le cast spécifié n'est pas valide.
+            //Skybound.Gecko.Xpcom.Initialize(@"C:\Program Files\Mozilla Firefox"); // FAIL: Le cast spécifié n'est pas valide.
+
+            foreach (string arg in Environment.GetCommandLineArgs())
+            {
+                if (System.String.Compare(arg, "-notitle", true) == 0)
+                    noTitle = true;
+            }
+
+            InitializeComponent();
+            webBrowser1 = new GeckoWebBrowser();
+            webBrowser1.Parent = this;
+            webBrowser1.Dock = DockStyle.Fill;
+
+            hook = new Hooks();
+
+            //hook.KeyPress += new KeyPressEventHandler(hook_KeyDown);
+            hook.KeyDown += new KeyEventHandler(hook_KeyDown);
+            //hook.KeyUp += new KeyEventHandler(hook_KeyDown);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("www.jango.com");
+        }
+
+        //Manage hook keys
+        void hook_KeyDown(object sender, KeyEventArgs e)
+        {
+            int now = Environment.TickCount;
+
+            if (e.KeyData == Keys.MediaPlayPause || (e.Control && e.Alt && e.KeyCode == Keys.P)/*.GetHashCode()*/)
+            {
+                //_jp.ctrls.onPlayPause(); ; return false;
+                //webBrowser1.Navigate("javascript:top.player.onPlayPause();");
+                webBrowser1.Navigate("javascript:_jp.ctrls.onPlayPause();");
+            }
+
+            if (e.KeyData == Keys.MediaNextTrack || (e.Control && e.Alt && e.KeyCode == Keys.N))
+            {
+                //_jp.ctrls.onSkip(); ; return false;
+                //webBrowser1.Navigate("javascript:top.player.onSkip(true);");
+                webBrowser1.Navigate("javascript:_jp.ctrls.onSkip();");
+                //webBrowser1.Navigate("javascript:top.player.setTimeout('top.player.onSkip(true)',0);");
+            }
+
+            if (e.KeyData == Keys.VolumeDown || e.KeyData == Keys.MediaStop)
+            {
+                if (now - lastVolumeChange > 100)
+                {
+                    volume -= 5;
+                    if (volume < 0)
+                    {
+                        volume = 0;
+                    }
+                    lastVolumeChange = Environment.TickCount;
+                    //webBrowser1.Navigate("javascript:top.player.onVolume(" + volume + ");");
+                    webBrowser1.Navigate("javascript:_jp.ctrls.onVolume(" + volume + ");");
+                }
+            }
+
+            if (e.KeyData == Keys.VolumeUp || e.KeyData == Keys.MediaPreviousTrack)
+            {
+                if (now - lastVolumeChange > 100)
+                {
+                    volume += 5;
+                    if (volume > 100)
+                    {
+                        volume = 100;
+                    }
+                    lastVolumeChange = Environment.TickCount;
+                    //webBrowser1.Navigate("javascript:top.player.onVolume(" + volume + ");");
+                    webBrowser1.Navigate("javascript:_jp.ctrls.onVolume(" + volume + ");");
+                }
+            }
+
+            /*            if (e.Control && e.Alt && e.KeyData == Keys.A)
+                        {
+                            MessageBox.Show("ctrl+alt+a");
+                        }
+            */
+        }
+
+        private void update_Tick(object sender, EventArgs e)
+        {
+            //Update the title
+            if (noTitle == false)
+            {
+                try
+                {
+                    /*if (webBrowser1.Document.Title.Length > 0)
+                    {
+                        //byte[] unicodeBytes = Encoding.Unicode.GetBytes(webBrowser1.Document.Title);
+                        byte[] unicodeBytes1;
+                        if (webBrowser1.Document.Title[webBrowser1.Document.Title.Length - 1] != '\0')
+                        {
+                            unicodeBytes1 = Encoding.ASCII.GetBytes(webBrowser1.Document.Title + '\0');
+                        }
+                        else
+                        {
+                            unicodeBytes1 = Encoding.ASCII.GetBytes(webBrowser1.Document.Title);
+                        }
+                        //Text = Encoding.Convert(Encoding.Unicode, Encoding.ASCII, unicodeBytes);
+                        Text = Encoding.Unicode.GetString(unicodeBytes1);
+                    }*/
+                    Text = webBrowser1.DocumentTitle;
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        private void ReHook_Click(object sender, EventArgs e)
+        {
+            //Remove the old hook
+            hook.KeyDown -= new KeyEventHandler(hook_KeyDown);
+
+            //Reinstall it
+            //hook = new Hooks();
+            hook.Stop();
+            hook.Start();
+
+            hook.KeyDown += new KeyEventHandler(hook_KeyDown);
+        }
+    }
+}
