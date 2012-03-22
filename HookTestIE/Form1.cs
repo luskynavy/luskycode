5023877aae218269efd1e506f98f0a74d9c626cd
YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr <YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr>
2012-03-22 12:28:44 +0100

    11-05-02 sbrowser - code cleaning - bugfix pause

--- HookTestIE/Form1.cs
+++ HookTestIE/Form1.cs
@@ -16,15 +16,16 @@ namespace HookTest
 
         //private string BrowserExecutable = "C:\\Program Files (x86)\\Avant Browser\\avant.exe";
         //private string BrowserExecutable = "C:\\Program Files\\Avant Browser\\avant.exe";
-        private string BrowserExecutable = "C:\\Program Files\\Orca Browser\\orca.exe";
-
         //private string BrowserName = "Avant";
-        private string BrowserName = "Orca";
+
+        //private string BrowserExecutable = "C:\\Program Files\\Orca Browser\\orca.exe";
+        //private string BrowserName = "Orca";
+
+        private string BrowserExecutable = "C:\\Program Files\\SlimBrowser\\sbrowser.exe";
+        private string BrowserName = "sbrowser";
 
         public Form1()
         {
-            //ClearCache();
-
             //"http://www.jango.com/profiles/1972516?c=1&l=0";
             //System.Threading.Thread.Suspend();
 
@@ -36,8 +37,7 @@ namespace HookTest
             //Process JangoIE = Process.Start("IExplore.exe", "http://www.jango.com/profiles/1972516?c=1&l=0");
             //Process JangoIE = Process.Start("chrome.exe", "http://www.jango.com/profiles/1972516?c=1&l=0");
             //Process JangoIE = Process.Start("C:\\Program Files (x86)\\Crazy Browser\\Crazy Browser.exe", "http://www.jango.com/profiles/1972516?c=1&l=0");
-            //Process JangoIE = Process.Start("C:\\Program Files (x86)\\Avant Browser\\avant.exe", "http://www.jango.com/profiles/1972516?c=1&l=0");
-            
+            //Process JangoIE = Process.Start("C:\\Program Files (x86)\\Avant Browser\\avant.exe", "http://www.jango.com/profiles/1972516?c=1&l=0");            
             //Process JangoIE = Process.Start("IExplore.exe");
 
             
@@ -50,11 +50,26 @@ namespace HookTest
         
             Hooks hook = new Hooks();
 
-            hook.KeyDown += new KeyEventHandler(hook_KeyDown);
-
-            //webBrowser1.Refresh(System.Windows.Forms.WebBrowserRefreshOption.Normal);            
+            hook.KeyDown += new KeyEventHandler(hook_KeyDown);            
         }
 
+        //Manage hook keys
+        void hook_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.MediaPlayPause)
+            {
+                Pause();
+            }
+
+            if (e.KeyData == Keys.MediaNextTrack)
+            {
+                CloseBrowser();
+
+                StartBrowser();
+            }
+        }
+
+        //Suspend/Resume the browser, start the browser if not launched
         private void Pause()
         {
             Process[] Browser = Process.GetProcessesByName(BrowserName);
@@ -63,72 +78,47 @@ namespace HookTest
                 StartBrowser();
                 suspended = false;
             }
-
-            Process JangoBrowser = Browser[0];
-            //Process JangoBrowser = Process.GetProcessById(632);
-
-            int ret = 0;
-
-            if (JangoBrowser.ProcessName != "")
+            else
             {
-                foreach (ProcessThread pT in JangoBrowser.Threads)
-                {
-                    IntPtr ptrOpenThread = Win32.OpenThread(Win32.ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+                Process JangoBrowser = Browser[0];
+                //Process JangoBrowser = Process.GetProcessById(632);
 
-                    if (ptrOpenThread != null)
+                int ret = 0;
+
+                if (JangoBrowser.ProcessName != "")
+                {
+                    foreach (ProcessThread pT in JangoBrowser.Threads)
                     {
-                        if (suspended)
+                        IntPtr ptrOpenThread = Win32.OpenThread(Win32.ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+
+                        if (ptrOpenThread != null)
                         {
-                            ret = Win32.ResumeThread(ptrOpenThread);
-                        }
-                        else
-                        {
-                            ret = Win32.SuspendThread(ptrOpenThread);
+                            if (suspended)
+                            {
+                                ret = Win32.ResumeThread(ptrOpenThread);
+                            }
+                            else
+                            {
+                                ret = Win32.SuspendThread(ptrOpenThread);
+                            }
                         }
                     }
+
+                    suspended = !suspended;
                 }
-
-                suspended = !suspended;
             }
         }
 
-        void hook_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyData == Keys.MediaPlayPause)
-            {
-                Pause();
-                //webBrowser1.Navigate("javascript:top.player.onPlayPause();");            
-                //MessageBox.Show("pause");
-                //throw new NotImplementedException();
-            }
-
-            if (e.KeyData == Keys.MediaNextTrack)
-            {
-                CloseBrowser();
-
-                StartBrowser();
-
-                //webBrowser1.Navigate("javascript:top.player.onSkip(true);");
-                //webBrowser1.Navigate("javascript:top.player.setTimeout('top.player.onSkip(true)',0);");
-                //MessageBox.Show("pause");
-                //throw new NotImplementedException();
-            }
-
-/*            if (e.Control && e.Alt && e.KeyData == Keys.A)
-            {
-                MessageBox.Show("ctrl+alt+a");
-            }
-*/            
-        }
-
+        //Start the browser minimized
         private Process StartBrowser()
         {
             ProcessStartInfo startInfo = new ProcessStartInfo(BrowserExecutable);
-            //startInfo.WindowStyle = ProcessWindowStyle.Minimized;
+            startInfo.WindowStyle = ProcessWindowStyle.Minimized; //launching browser minimized make orca bug
 
             return Process.Start(startInfo);             
         }
 
+        //Close the browser
         private void CloseBrowser()
         {
             Process[] Browser = Process.GetProcessesByName(BrowserName);
@@ -149,8 +139,6 @@ namespace HookTest
         private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             CloseBrowser();
-            //webBrowser1.Navigate("about:blank");
-            //webBrowser1.Stop();
         }
     }
 }
