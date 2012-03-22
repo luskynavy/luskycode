1207dad87a0b4a7ef7535ef951ce4a9c19ce0955
YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr <YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr>
2012-03-22 10:44:33 +0100

    Added MySQL

--- TestSQL/TestSQL/Program.cs
+++ TestSQL/TestSQL/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 
 using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+//pour se connecter à mysql : http://dev.mysql.com/downloads/connector/net/6.4.4.html (le 1.0.10 ne s'enregistre pas et est vieux...)
+//ajout de la référence : C:\Program Files\MySQL\MySQL Connector Net 6.4.4\Assemblies\v2.0\MySql.Data.dll
 
 namespace TestSQL
 {
@@ -15,10 +19,42 @@ namespace TestSQL
             SqlCommand command = new SqlCommand();
 
             //connexion au serveur local en utilisant l'autentification windows
-            connexion.ConnectionString = "Integrated security = SSPI ; server = YKALAS01A8-01\\SQLEXPRESS; database = ";
+            connexion.ConnectionString = "Integrated security = SSPI ; server = YKALAS01A8-01\\SQLEXPRESS; database = ";            
 
             try
             {
+                int ret;
+
+                string MyConString = "SERVER=localhost;" +
+                "DATABASE=test;";/* +
+				"UID=testuser;" +
+				"PASSWORD=testpassword;";*/
+                MySqlConnection myconnection = new MySqlConnection(MyConString);
+                MySqlCommand mycommand = myconnection.CreateCommand();
+                MySqlDataReader mydatareader;
+                mycommand.CommandText = "SELECT * FROM table_1";
+                myconnection.Open();
+                mydatareader = mycommand.ExecuteReader();
+                while (mydatareader.Read())
+                {
+                    for (int i = 0; i < mydatareader.FieldCount; i++)
+                    {
+                        System.Console.Write(mydatareader.GetValue(i).ToString() + " ");
+                    }
+                    System.Console.WriteLine();
+                }
+                mydatareader.Close();
+
+                System.Console.WriteLine("\n for i:0->999 insert into table_1 (val, truc) values (i, '2')");
+                for (int i = 0; i < 1000; i++)
+                {
+                    mycommand.CommandText = "insert into table_1 (val, truc) values (" + i + ", '2')";
+                    ret = mycommand.ExecuteNonQuery();
+                    //on ferme le datareader pour pouvoir réexécuter une commande après
+                    mydatareader.Close();
+                }
+                myconnection.Close();
+
                 //on ouvre
                 connexion.Open();
                 command.Connection = connexion;
@@ -60,9 +96,20 @@ namespace TestSQL
                 //update d'une valeur
                 System.Console.WriteLine("\nUPDATE testdb.dbo.Table_1 SET val = 'a' + str(truc)  WHERE truc = 123");
                 command.CommandText = "UPDATE testdb.dbo.Table_1 SET val = 'a' + str(truc)  WHERE truc = 123";
-                int ret = command.ExecuteNonQuery();
+                ret = command.ExecuteNonQuery();
                 //on ferme le datareader pour pouvoir réexécuter une commande après
                 datareader.Close();
+
+                //update d'une valeur
+                System.Console.WriteLine("\n for i:0->999 insert into [testdb].[dbo].Table_1 (val, truc) values (i, '2')");
+                for (int i = 0; i < 1000; i++)
+                {
+                    command.CommandText = "insert into [testdb].[dbo].Table_1 (val, truc) values (" + i + ", '2')";
+                    ret = command.ExecuteNonQuery();
+                    //on ferme le datareader pour pouvoir réexécuter une commande après
+                    datareader.Close();
+                }
+                
 
                 //test des valeurs d'une table
                 System.Console.WriteLine("\nSELECT * FROM testdb.dbo.Table_1");
