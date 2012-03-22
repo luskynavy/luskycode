f7261982c24938abfa06bdf84be12f5b3ff991b5
YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr <YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr>
2012-03-22 11:41:45 +0100

    creation

---
+++ HookTest/HookTest/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HookTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Hooks hook = new Hooks();
+
+            hook.KeyDown += new KeyEventHandler(hook_KeyDown);
+
+            //webBrowser1.Refresh(System.Windows.Forms.WebBrowserRefreshOption.Normal);            
+        }
+
+        void hook_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.MediaPlayPause)
+            {
+                webBrowser1.Navigate("javascript:top.player.onPlayPause();");            
+                //MessageBox.Show("pause");
+                //throw new NotImplementedException();
+            }
+
+            if (e.KeyData == Keys.MediaNextTrack)
+            {
+                webBrowser1.Navigate("javascript:top.player.onSkip(true);");
+                //webBrowser1.Navigate("javascript:top.player.setTimeout('top.player.onSkip(true)',0);");
+                //MessageBox.Show("pause");
+                //throw new NotImplementedException();
+            }
+
+/*            if (e.Control && e.Alt && e.KeyData == Keys.A)
+            {
+                MessageBox.Show("ctrl+alt+a");
+            }
+*/            
+        }
+
+
+    }
+}
