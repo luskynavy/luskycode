925d9b3573b39bede32e7403d86c23062718957d
YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr <YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr>
2012-03-22 11:05:35 +0100

    11-07-16 creation

---
+++ RandWallpaper/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using System.Runtime.InteropServices;
+
+
+namespace RandWallpaper
+{
+    public static class Win32 
+    {
+        [DllImport("user32.dll", CharSet=CharSet.Auto)]
+        public static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ;
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() == 1)
+            {
+                DirectoryInfo dir = new DirectoryInfo(args[0]);
+                FileInfo[] files = null;
+                try
+                {
+                    files = dir.GetFiles();
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    Console.WriteLine("Invalid directory: " + args[0]);
+                }
+
+                //if directory exists and there are files
+                if (files != null && files.Count() > 0)
+                {
+                    Random rand = new Random();
+                    int i = rand.Next(files.Count());
+
+                    string wallpaper = args[0] + @"\" + files[i].Name;
+
+                    int nResult = Win32.SystemParametersInfo(20/*SPI_SETDESKWALLPAPER*/, 0, wallpaper, 0x1 | 0x2);
+                    if (nResult != 1)
+                    {
+                        Console.WriteLine("SystemParametersInfo SPI_SETDESKWALLPAPER failed for " + wallpaper);
+                    }
+                    else
+                    {
+                        Console.WriteLine(wallpaper + " was choosen.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("You must specify a directory as argument.");
+            }
+        }
+    }
+}
---
+++ RandWallpaper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RandWallpaper")]
+[assembly: AssemblyDescription("Set a random wallpaper given a directory")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lusky Company")]
+[assembly: AssemblyProduct("RandWallpaper")]
+[assembly: AssemblyCopyright("Copyright © 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2600886c-1281-4885-a297-8cd50f91a627")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("11.07.16.0")]
