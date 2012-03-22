a09e26a4ab83a37f630be70a4e1ca34347deca54
YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr <YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr>
2012-03-22 12:39:02 +0100

    Mandel CPP CSHARP 11-12-16 solution creation

---
+++ Mandel/Mandel.cpp
@@ -0,0 +1,68 @@
+// Mandel.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include "windows.h"
+#include "math.h"
+
+
+double CurrentSecond()
+{
+	LARGE_INTEGER current;
+	QueryPerformanceCounter(&current);
+	LONGLONG frequency;
+	QueryPerformanceFrequency( (LARGE_INTEGER *)&frequency);
+	return (double)current.QuadPart / (double)frequency;
+
+}
+
+#define STEPS_MAX 3000
+#define TYPE_CALC double
+//#define TYPE_CALC float
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int steps = STEPS_MAX;
+	TYPE_CALC xmin = -2;
+	TYPE_CALC ymin = -1;
+	TYPE_CALC xmax = 1;
+	TYPE_CALC ymax = 1;
+	TYPE_CALC xstep = (xmax - xmin) / steps;
+	TYPE_CALC ystep = (ymax - ymin) / steps;	
+
+	double starttime = CurrentSecond();
+	int total = 0;
+
+	int* mandel = new int[STEPS_MAX * STEPS_MAX];
+
+	int xmandel = 0;
+	int ymandel = 0;
+	for (TYPE_CALC x = xmin; xmandel < steps; x += xstep, xmandel++)
+	{		
+		ymandel = 0;
+		for (TYPE_CALC y = ymin; ymandel < steps; y += ystep, ymandel++)
+		{
+			TYPE_CALC x1 = 0;
+			TYPE_CALC y1 = 0;
+			int looper = 0;
+			while (looper < 100 && sqrt((x1 * x1) + (y1 * y1)) < 2)
+			{
+				looper++;
+				TYPE_CALC xx = (x1 * x1) - (y1 * y1) + x;
+				y1 = 2 * x1 * y1 + y;
+				x1 = xx;
+			}
+			total += looper;
+			mandel[xmandel + ymandel * steps] = looper;
+		}
+	}
+
+	double time = CurrentSecond() - starttime;
+
+	printf("mandel 500 200 %d\n", mandel[500 + 200 * steps]);
+	printf("total %d\n", total);
+	printf("time %.3f\n", time);
+
+	return 0;
+}
+
---
+++ MandelCSharp/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MandelCSharp
+{
+    using TYPE_CALC = System.Double;
+    //using TYPE_CALC = System.Single;
+
+    class Program
+    {
+        [System.Runtime.InteropServices.DllImport("KERNEL32")]
+        private static extern bool QueryPerformanceCounter(ref long lpPerformanceCount);
+
+        [System.Runtime.InteropServices.DllImport("KERNEL32")]
+        private static extern bool QueryPerformanceFrequency(ref long lpFrequency);
+
+
+        public static double CurrentSecond
+        {
+            get
+            {
+                long current = 0;
+                QueryPerformanceCounter(ref current);
+                long frequency = 0;
+                QueryPerformanceFrequency(ref frequency);
+                return (double)current / (double)frequency;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            double starttime = CurrentSecond;
+
+            int steps = 3000;
+            TYPE_CALC xmin = -2;
+            TYPE_CALC ymin = -1;
+            TYPE_CALC xmax = 1;
+            TYPE_CALC ymax = 1;
+            TYPE_CALC xstep = (xmax - xmin) / steps;
+            TYPE_CALC ystep = (ymax - ymin) / steps;
+            
+            int total = 0;
+
+            int[] mandel = new int[steps * steps];
+
+            int xmandel = 0;
+            int ymandel = 0;
+            for (TYPE_CALC x = xmin; xmandel < steps; x += xstep, xmandel++)
+            {
+                ymandel = 0;
+                for (TYPE_CALC y = ymin; ymandel < steps; y += ystep, ymandel++)
+                {
+                    TYPE_CALC x1 = 0;
+                    TYPE_CALC y1 = 0;
+                    int looper = 0;
+                    while (looper < 100 && Math.Sqrt((x1 * x1) + (y1 * y1)) < 2)
+                    {
+                        looper++;
+                        TYPE_CALC xx = (x1 * x1) - (y1 * y1) + x;
+                        y1 = 2 * x1 * y1 + y;
+                        x1 = xx;
+                    }
+                    total += looper;
+                    mandel[xmandel + ymandel * steps] = looper;
+                }
+            }
+
+            double time = CurrentSecond - starttime;
+
+            Console.WriteLine("mandel  500 200 " + mandel[500 + 200 * steps]);
+
+            Console.WriteLine("total " + total);
+
+            Console.WriteLine("time " + time);
+        }
+    }
+}
