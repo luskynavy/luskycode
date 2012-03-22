496d1125e9f65c95f64751a392dac974fe909388
YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr <YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr>
2012-03-22 11:23:14 +0100

    09-09-27 creation

---
+++ StuffCounter/StuffCounter/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace StuffCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.go = new System.Windows.Forms.Button();
+            this.realm = new System.Windows.Forms.TextBox();
+            this.guild = new System.Windows.Forms.TextBox();
+            this.results = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // go
+            // 
+            this.go.Dock = System.Windows.Forms.DockStyle.Right;
+            this.go.Location = new System.Drawing.Point(253, 0);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(75, 24);
+            this.go.TabIndex = 0;
+            this.go.Text = "Go";
+            this.go.UseVisualStyleBackColor = true;
+            this.go.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // realm
+            // 
+            this.realm.Dock = System.Windows.Forms.DockStyle.Left;
+            this.realm.Location = new System.Drawing.Point(0, 0);
+            this.realm.Name = "realm";
+            this.realm.Size = new System.Drawing.Size(100, 20);
+            this.realm.TabIndex = 1;
+            this.realm.Text = "Eldre\'Thalas";
+            // 
+            // guild
+            // 
+            this.guild.Location = new System.Drawing.Point(127, 0);
+            this.guild.Name = "guild";
+            this.guild.Size = new System.Drawing.Size(100, 20);
+            this.guild.TabIndex = 1;
+            this.guild.Text = "Ancesträl";
+            // 
+            // results
+            // 
+            this.results.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.results.Location = new System.Drawing.Point(0, 30);
+            this.results.Multiline = true;
+            this.results.Name = "results";
+            this.results.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.results.Size = new System.Drawing.Size(558, 314);
+            this.results.TabIndex = 2;
+            this.results.Text = "Results";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.go);
+            this.panel1.Controls.Add(this.guild);
+            this.panel1.Controls.Add(this.realm);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(328, 24);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 344);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.results);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button go;
+        private System.Windows.Forms.TextBox realm;
+        private System.Windows.Forms.TextBox guild;
+        private System.Windows.Forms.TextBox results;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ StuffCounter/StuffCounter/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Xml;
+
+namespace StuffCounter
+{
+    public partial class Form1 : Form
+    {
+        //5 seems to be the magic number when the armory is acting up.
+        private const int RETRY_MAX = 5;
+        string UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.8.1.4) Gecko/20070515 Firefox/2.0.0.4";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static Exception _fatalError = null;
+
+        /// <summary>
+        /// If the last request received a 407 or no response. Used to prevent a lot of bad calls.
+        /// It also has the good side effect of not locking someone's account out if they enter the proxy info incorrectly
+        /// by sending lots of bad authorization attempts.
+        /// </summary>
+        public static bool LastWasFatalError
+        {
+            get { return _fatalError != null; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlDocument xml = DownloadXml("http://eu.wowarmory.com/guild-info.xml?r=Eldre%27Thalas&gn=Ancestr%C3%A4l");
+            results.Text = xml.InnerXml;
+        }
+
+        /// <summary>
+		/// This is used to prevent multiple attempts at network traffic when its not working and 
+		/// continuing to issue requests could cause serious problems for the user.
+		/// </summary>
+		/// <param name="ex"></param>
+		private void CheckExceptionForFatalError(Exception ex)
+		{
+			//Log.Write("Exception trying to download: "+ ex);
+            //Log.Write(ex.StackTrace);
+			if (ex.Message.Contains("407") /*proxy auth required */
+				|| ex.Message.Contains("403") /*proxy info probably wrong, if we keep issuing requests, they will probably get locked out*/
+				|| ex.Message.Contains("timed out") /*either proxy required and firewall dropped the request, or armory is down*/
+				//|| ex.Message.Contains("invalid content type") /*unexpected content type returned*/
+				|| ex.Message.Contains("The remote name could not be resolved") /* DNS problems*/
+                )
+			{
+				_fatalError = ex;
+			}
+		}
+
+        /// <summary>
+        /// Used to create a web client with all of the appropriote proxy/useragent/etc settings
+        /// </summary>
+        private WebClient CreateWebClient()
+        {
+            WebClient client = new WebClient() { Encoding = Encoding.UTF8 };
+            client.Headers.Add("user-agent", UserAgent);
+           /* if (NetworkSettingsProvider.ProxyType == "Http")
+            {
+                //if (_useDefaultProxy)
+                {
+                    client.Proxy = HttpWebRequest.DefaultWebProxy;
+                }
+                else if (!String.IsNullOrEmpty(_proxyServer))
+                {
+                    client.Proxy = new WebProxy(_proxyServer, _proxyPort);
+                }
+                if (client.Proxy != null && NetworkSettingsProvider.ProxyRequiresAuthentication)
+                {
+                    if (NetworkSettingsProvider.UseDefaultAuthenticationForProxy)
+                    {
+                        client.Proxy.Credentials = CredentialCache.DefaultNetworkCredentials;
+                    }
+                    else
+                    {
+                        client.Proxy.Credentials = new NetworkCredential(_proxyUserName, _proxyPassword, _proxyDomain);
+                    }
+                }
+            }*/
+            return client;
+        }
+
+        public string DownloadText(string URI)
+        {
+            WebClient webClient = CreateWebClient();
+            string value = null;
+            int retry = 0;
+            bool success = false;
+            do
+            {
+                if (!LastWasFatalError)
+                {
+                    try
+                    {
+                        value = webClient.DownloadString(URI);
+                        if (!String.IsNullOrEmpty(value))
+                        {
+                            success = true;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        CheckExceptionForFatalError(ex);
+                    }
+                }
+                retry++;
+            } while (retry <= RETRY_MAX && !success && !LastWasFatalError);
+            return value;
+        }
+
+        private XmlDocument DownloadXml(string URI) { return DownloadXml(URI, false); }
+        private XmlDocument DownloadXml(string URI, bool allowTable)
+        {
+            XmlDocument returnDocument = null;
+            int retry = 0;
+            //Download Text has retry logic in it as well, but that just makes sure it gets a response, this
+            //makes sure we get a valid XML response.
+            do
+            {
+                string xml = DownloadText(URI);
+                //If it contains "<table", then the armory accidentally returned it as html instead of xml.
+                if (!string.IsNullOrEmpty(xml) && (allowTable || !xml.Contains("<table")))
+                {
+                    try
+                    {
+                        returnDocument = new XmlDocument();
+                        returnDocument.XmlResolver = null;
+                        returnDocument.LoadXml(xml.Replace("&", ""));
+                        if (returnDocument == null || returnDocument.DocumentElement == null
+                                    || !returnDocument.DocumentElement.HasChildNodes
+                            /*|| !returnDocument.DocumentElement.ChildNodes[0].HasChildNodes*/) // this check is no longer valid
+                        {
+                            //document returned no data we care about.
+                            returnDocument = null;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                }
+                retry++;
+            } while (returnDocument == null && !LastWasFatalError && retry < RETRY_MAX);
+
+            return returnDocument;
+        }
+    }
+}
