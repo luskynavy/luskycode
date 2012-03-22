d3478d4ad361674bf08721f5b89c39e2bca4d95c
YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr <YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr>
2012-03-22 11:51:08 +0100

    creation

---
+++ WhoIs/WhoIs/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WhoIs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.textBoxIp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxServerIp = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxServerPort = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.labelNslookup = new System.Windows.Forms.Label();
+            this.textBoxNslookup = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(517, 21);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(75, 23);
+            this.buttonGo.TabIndex = 4;
+            this.buttonGo.Text = "Go";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // textBoxIp
+            // 
+            this.textBoxIp.Location = new System.Drawing.Point(12, 25);
+            this.textBoxIp.Name = "textBoxIp";
+            this.textBoxIp.Size = new System.Drawing.Size(204, 20);
+            this.textBoxIp.TabIndex = 1;
+            this.textBoxIp.Text = "google.fr";            
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Domain or ip:";
+            // 
+            // textBoxServerIp
+            // 
+            this.textBoxServerIp.Location = new System.Drawing.Point(222, 25);
+            this.textBoxServerIp.Name = "textBoxServerIp";
+            this.textBoxServerIp.Size = new System.Drawing.Size(180, 20);
+            this.textBoxServerIp.TabIndex = 2;
+            this.textBoxServerIp.Text = "whois.ripe.net";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(219, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Using whois server:";
+            // 
+            // textBoxServerPort
+            // 
+            this.textBoxServerPort.Location = new System.Drawing.Point(408, 24);
+            this.textBoxServerPort.Name = "textBoxServerPort";
+            this.textBoxServerPort.Size = new System.Drawing.Size(100, 20);
+            this.textBoxServerPort.TabIndex = 3;
+            this.textBoxServerPort.Text = "43";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(405, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "On port:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Result:";
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxResult.HideSelection = false;
+            this.textBoxResult.Location = new System.Drawing.Point(12, 123);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxResult.Size = new System.Drawing.Size(580, 353);
+            this.textBoxResult.TabIndex = 6;
+            // 
+            // labelNslookup
+            // 
+            this.labelNslookup.AutoSize = true;
+            this.labelNslookup.Location = new System.Drawing.Point(9, 58);
+            this.labelNslookup.Name = "labelNslookup";
+            this.labelNslookup.Size = new System.Drawing.Size(59, 13);
+            this.labelNslookup.TabIndex = 5;
+            this.labelNslookup.Text = "NsLookup:";
+            // 
+            // textBoxNslookup
+            // 
+            this.textBoxNslookup.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxNslookup.HideSelection = false;
+            this.textBoxNslookup.Location = new System.Drawing.Point(12, 74);
+            this.textBoxNslookup.Name = "textBoxNslookup";
+            this.textBoxNslookup.ReadOnly = true;
+            this.textBoxNslookup.Size = new System.Drawing.Size(580, 20);
+            this.textBoxNslookup.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 488);
+            this.Controls.Add(this.textBoxNslookup);
+            this.Controls.Add(this.labelNslookup);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxServerPort);
+            this.Controls.Add(this.textBoxServerIp);
+            this.Controls.Add(this.textBoxIp);
+            this.Controls.Add(this.buttonGo);
+            this.Name = "Form1";
+            this.Text = "WhoIs";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.TextBox textBoxIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxServerIp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxServerPort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Label labelNslookup;
+        private System.Windows.Forms.TextBox textBoxNslookup;
+    }
+}
+
---
+++ WhoIs/WhoIs/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Net.Sockets;
+
+using System.Net;
+
+// Who is de Romain
+// http://www.netreport.fr/whois/redirect.asp?ip=82.67.16.100<asp>
+
+// Mapping IP addresses to country codes
+// http://www.codeproject.com/KB/cs/iplookupoptimise.aspx?msg=502031
+
+
+namespace WhoIs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Source : http://www.asp-php.net/ressources/bouts_de_code.aspx?id=961
+
+        /// <summary>
+        /// Interroge un serveur Whois pour le nom de domaine spécifié.
+        /// </summary>
+        /// <param name="domain">Nom de domaine à vérifier.</param>
+        /// <param name="server">Adresse du serveur Whois à interroger.</param>
+        /// <param name="port">Port du serveur Whois à interroger.</param>
+        /// <returns>Résultat de l'interrogation du serveur Whois, ou une référence null en cas d'erreur.</returns>
+        /// <exception cref="ArgumentException"><paramref name="domain" /> ou <paramref name="server" /> est vide.</exception>
+        public static string QueryWhoisServer(string domain, string server, int port)
+        {
+            // Validation des arguments
+            //if (string.IsNullOrEmpty(domain)) throw new ArgumentException("Domain name can't be empty", "domain");
+            //if (string.IsNullOrEmpty(server)) throw new ArgumentException("Whois server can't be empty", "server");
+
+            try
+            {
+                // Ouverture de la connexion au serveur : les services Whois utilisent en général le port 43
+                using (var client = new TcpClient(server, port))
+                {
+                    // Ouverture du flux de données
+                    using (var stream = client.GetStream())
+                    {
+                        // Envoi de la requête Whois
+                        var query = Encoding.ASCII.GetBytes(domain + "\r\n");
+                        stream.Write(query, 0, query.Length);
+
+                        // Lecture de la réponse
+                        using (var reader = new StreamReader(stream, Encoding.ASCII)) return reader.ReadToEnd().Trim();
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        private void Go()
+        {
+            // NsLookup
+            IPHostEntry ipEntry;
+
+            try
+            {
+                ipEntry = Dns.GetHostEntry(textBoxIp.Text);
+                textBoxNslookup.Text = ipEntry.HostName + ": ";
+                for (int i = 0; i < ipEntry.AddressList.Length; i++)
+                {
+                    if (i > 0)
+                    {
+                        textBoxNslookup.Text += ", ";
+                    }
+                    textBoxNslookup.Text += ipEntry.AddressList[i];
+                }
+            }
+            catch (Exception ex)
+            {
+                textBoxNslookup.Text = ex.Message;
+            }
+            
+
+            // Do the who is
+            string result = QueryWhoisServer(textBoxIp.Text, textBoxServerIp.Text, 43/*0 + textBoxServerPort.Text*/);
+            if (result != null)
+            {
+                string resultNewLine = result.Replace("\n", "\r\n");
+                textBoxResult.Text = resultNewLine;
+            }
+            else
+            {
+                textBoxResult.Text = "null";
+            }            
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            Go();
+        }                
+    }
+}
+
+
