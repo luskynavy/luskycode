e8a819f495f9eb417de1ca70d952bde0b3e2f1da
YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr <YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr>
2012-03-22 12:19:01 +0100

    11-04-05 bugfix rename and  infos

---
+++ JangoPlayer/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace HookTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.update = new System.Windows.Forms.Timer(this.components);
+            this.ReHook = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.AllowWebBrowserDrop = false;
+            this.webBrowser1.CausesValidation = false;
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1003, 288);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("http://www.jango.com/profiles/1972516?c=1&l=0", System.UriKind.Absolute);
+            // 
+            // update
+            // 
+            this.update.Enabled = true;
+            this.update.Interval = 1000;
+            this.update.Tick += new System.EventHandler(this.update_Tick);
+            // 
+            // ReHook
+            // 
+            this.ReHook.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ReHook.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.ReHook.Location = new System.Drawing.Point(0, 0);
+            this.ReHook.Name = "ReHook";
+            this.ReHook.Size = new System.Drawing.Size(23, 23);
+            this.ReHook.TabIndex = 2;
+            this.ReHook.Text = "R";
+            this.ReHook.UseVisualStyleBackColor = false;
+            this.ReHook.Click += new System.EventHandler(this.ReHook_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 288);
+            this.Controls.Add(this.ReHook);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Closing += new System.ComponentModel.CancelEventHandler(this.Form1_Closing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Timer update;
+        private System.Windows.Forms.Button ReHook;
+    }
+}
+
---
+++ JangoPlayer/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HookTest
+{
+    public partial class Form1 : Form
+    {
+        public int volume = 50;
+        public int lastVolumeChange = 0;
+        public bool noTitle = false;
+        public Hooks hook;
+
+
+        public Form1()
+        {
+            bool scriptErrorsOption = false;
+            bool clearCacheOption = true;
+
+            foreach (string arg in Environment.GetCommandLineArgs())
+            {
+                if (System.String.Compare(arg, "-noerrors", true) == 0)
+                    scriptErrorsOption = true;
+
+                if (System.String.Compare(arg, "-noclearcache", true) == 0)
+                    clearCacheOption = false;
+
+                if (System.String.Compare(arg, "-notitle", true) == 0)
+                    noTitle = true;
+            }
+
+            if (clearCacheOption)
+                ClearCache();
+
+            InitializeComponent();
+
+            if (scriptErrorsOption)
+                webBrowser1.ScriptErrorsSuppressed = true;
+
+            hook = new Hooks();
+
+            //hook.KeyPress += new KeyPressEventHandler(hook_KeyDown);
+            hook.KeyDown += new KeyEventHandler(hook_KeyDown);
+            //hook.KeyUp += new KeyEventHandler(hook_KeyDown);
+
+            //webBrowser1.Refresh(System.Windows.Forms.WebBrowserRefreshOption.Normal);
+        }
+
+        void hook_KeyDown(object sender, KeyEventArgs e)
+        //void hook_KeyDown(object sender, KeyPressEventArgs e)
+        {
+            //int key = e.GetHashCode();
+            Keys key = e.KeyData;
+            int now = Environment.TickCount;
+                
+
+            if (key == Keys.MediaPlayPause/*.GetHashCode()*/)
+            {
+                //_jp.ctrls.onPlayPause(); ; return false;
+                //webBrowser1.Navigate("javascript:top.player.onPlayPause();");
+                webBrowser1.Navigate("javascript:_jp.ctrls.onPlayPause();");
+            }
+
+            if (key == Keys.MediaNextTrack/*.GetHashCode()*/)
+            {
+                //_jp.ctrls.onSkip(); ; return false;
+                //webBrowser1.Navigate("javascript:top.player.onSkip(true);");
+                webBrowser1.Navigate("javascript:_jp.ctrls.onSkip();");
+                //webBrowser1.Navigate("javascript:top.player.setTimeout('top.player.onSkip(true)',0);");
+            }
+            
+            if (key == Keys.VolumeDown/*.GetHashCode()*/ || key == Keys.MediaStop/*.GetHashCode()*/)
+            {
+                if (now - lastVolumeChange > 100)
+                {
+                    volume -= 5;
+                    if (volume < 0)
+                    {
+                        volume = 0;
+                    }
+                    lastVolumeChange = Environment.TickCount;
+                    //webBrowser1.Navigate("javascript:top.player.onVolume(" + volume + ");");
+                    webBrowser1.Navigate("javascript:_jp.ctrls.onVolume(" + volume + ");");
+                }
+            }
+
+            if (key == Keys.VolumeUp/*.GetHashCode()*/ || key == Keys.MediaPreviousTrack/*.GetHashCode()*/)
+            {
+                if (now - lastVolumeChange > 100)
+                {
+                    volume += 5;
+                    if (volume > 100)
+                    {
+                        volume = 100;
+                    }
+                    lastVolumeChange = Environment.TickCount;
+                    //webBrowser1.Navigate("javascript:top.player.onVolume(" + volume + ");");
+                    webBrowser1.Navigate("javascript:_jp.ctrls.onVolume(" + volume + ");");
+                }
+            }
+
+/*            if (e.Control && e.Alt && e.KeyData == Keys.A)
+            {
+                MessageBox.Show("ctrl+alt+a");
+            }
+*/            
+        }
+
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            webBrowser1.Navigate("about:blank");
+            //webBrowser1.Stop();
+        }
+
+        private void update_Tick(object sender, EventArgs e)
+        {
+            //Update the title
+            if (noTitle == false)
+            {
+                try
+                {
+                    Text = webBrowser1.Document.Title;
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        private void ReHook_Click(object sender, EventArgs e)
+        {
+            //Remove the old hook
+            hook.KeyDown -= new KeyEventHandler(hook_KeyDown);
+
+            //Reinstall it
+            hook = new Hooks();
+            
+            hook.KeyDown += new KeyEventHandler(hook_KeyDown);
+        }
+    }
+}
