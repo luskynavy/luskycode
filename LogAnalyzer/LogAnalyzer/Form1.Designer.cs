4930ce271a4d8c3dfff1b8b1d18062c201bedc7b
YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr <YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr>
2012-03-22 11:54:06 +0100

    creation

---
+++ LogAnalyzer/LogAnalyzer/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace LogAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.results = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // results
+            // 
+            this.results.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.results.FullRowSelect = true;
+            this.results.GridLines = true;
+            this.results.Location = new System.Drawing.Point(12, 35);
+            this.results.Name = "results";
+            this.results.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.results.Size = new System.Drawing.Size(705, 403);
+            this.results.TabIndex = 0;
+            this.results.UseCompatibleStateImageBehavior = false;
+            this.results.View = System.Windows.Forms.View.Details;
+            this.results.DoubleClick += new System.EventHandler(this.results_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Analyze";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(586, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(685, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(31, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.results);
+            this.Name = "Form1";
+            this.Text = "Log Analyzer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView results;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ LogAnalyzer/LogAnalyzer/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+/*
+ How to detect platform and player name
+
+PS3
+---
+sceNpManagerGetAccountRegion
+[GMLogin] eden121 signed in
+
+PC
+--
+<UPnP> GetPortMapping
+<edLive> TRACE : edLiveGroupHandler::AddUsersFromSave() _local_user: 1102776, "UnLuskyDePlus", 1102776 )
+<edLive> TRACE : edLiveGroupHandler::AddFriendsFromLocalProfile( 0 ) _local_user: 1102776, "UnLuskyDePlus", 1102776 )
+
+360
+---
+XNADDR
+[GMLogin] edenFabEuro signed in
+
+*/ 
+
+namespace LogAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        //string searchDir = @"C:\Users\YVANKA~1\AppData\Local\Temp\DWRCC Downloads\LEMON 2010-08-05 2010-08-06";
+        string searchDir = Directory.GetCurrentDirectory();
+        string filesFilter = "*.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = searchDir;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int sizeToRead = 65535;
+
+            //results.BeginUpdate();
+
+            results.Clear();            
+
+            results.Columns.Add("Name"/*, -1/*, -2, HorizontalAlignment.Left */);
+            results.Columns.Add("Size"/*, -2, HorizontalAlignment.Right/*, -2, HorizontalAlignment.Right*/);
+            results.Columns.Add("Infos"/*, -1/*, -2, HorizontalAlignment.Left*/);
+
+            //string[] files = System.IO.Directory.GetFiles(searchDir, filesFilter);
+            DirectoryInfo dir = new DirectoryInfo(searchDir);
+            FileInfo[] files = dir.GetFiles(filesFilter);
+            foreach (FileInfo f in files)
+            {
+                byte[] b = new byte[sizeToRead];
+                UTF8Encoding temp = new UTF8Encoding(true);
+
+                FileStream fs = File.OpenRead(f.FullName);                
+                fs.Read(b, 0, b.Length);
+                fs.Close();
+
+                string text = temp.GetString(b);
+
+                string type = "Unknown";
+                bool is360 = (text.IndexOf("XNADDR") != -1);
+                if (is360)
+                {
+                    type = "360";
+                }
+
+                bool isPC = false;
+                if (!is360)
+                {
+                    isPC = (text.IndexOf("<UPnP> GetPortMapping") != -1);
+                    if (isPC)
+                    {
+                        type = "PC";
+                    }                    
+                }
+
+                bool isPS3 = false;
+                if (!is360 && !isPC)
+                {
+                    isPS3 = (text.IndexOf("sceNpManagerGetAccountRegion") != -1);
+                    if (!isPS3)
+                    {
+                        isPS3 = (text.IndexOf("CELL_SYSUTIL_") != -1);                        
+                    }
+                    if (isPS3)
+                    {
+                        type = "PS3";
+                    }
+                }                
+
+                 ListViewItem item1 = new ListViewItem(f.Name);               
+                 item1.SubItems.Add("" + f.Length);
+                 item1.SubItems.Add(type);
+                 results.Items.Add(item1);
+            }
+
+            results.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+
+            //results.EndUpdate();
+        }
+
+        private void results_DoubleClick(object sender, EventArgs e)
+        {
+            string name = results.SelectedItems[0].Text;
+            System.Diagnostics.Process.Start("explorer.exe", searchDir + "\\" + name);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderFileDialog1 = new FolderBrowserDialog();
+            folderFileDialog1.SelectedPath = searchDir;
+
+            if (folderFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                searchDir = folderFileDialog1.SelectedPath;
+                textBox1.Text = searchDir;
+                //openFileDialog1.FileName;
+            }
+
+        }
+    }
+}
