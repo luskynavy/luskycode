b4fc31e86f10702f9b74112cc333c0c15a72a5ea
YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr <YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr>
2012-04-23 10:14:50 +0200

    ServiceTest 12-04-23 creation

---
+++ ServiceTest/ServiceTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ServiceProcess;
+
+
+namespace ServiceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceController[] scServices;
+            scServices = ServiceController.GetServices();
+
+            foreach (ServiceController scTemp in scServices)
+            {
+                Console.WriteLine(scTemp.ServiceName + " : " + scTemp.Status
+                    + (scTemp.CanPauseAndContinue ? " : CanPauseAndContinue" : ""));
+            }
+        }
+    }
+}
