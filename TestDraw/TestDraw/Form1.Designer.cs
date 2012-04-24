0bcb9f8e4536519c85447fc9ef3d85b2b879595f
YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr <YKALAF01A8@YKALAS01A8-01.lyon.t-systems.fr>
2012-04-24 15:31:56 +0200

    TestDraw 12-04-23 creation

---
+++ TestDraw/TestDraw/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace TestDraw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "mangifique bouton ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "rien à lire c\'est juste du texte pour voir";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(91, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "rien à lire c\'est juste du texte pour voir";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 521);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TestDraw/TestDraw/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Drawing.Drawing2D;
+
+namespace TestDraw
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            Graphics formGraphics = this.CreateGraphics();
+            formGraphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+            Pen myPen;
+            myPen = new Pen(Color.Red, 2);
+            
+            formGraphics.DrawRectangle(myPen, new Rectangle(0, 0, 200, 300));
+            
+            formGraphics.FillRectangle(Brushes.Orange, new Rectangle(25, 50, 100, 200));
+
+            myPen.Width = 1;
+            myPen.DashStyle = DashStyle.DashDot;
+
+            formGraphics.DrawRectangle(myPen, new Rectangle(1, 1, 50, 50));
+
+            Brush myBrush;
+            myBrush = new HatchBrush(HatchStyle.SolidDiamond,
+               Color.Red, Color.Black);
+            formGraphics.FillRectangle(myBrush, new Rectangle(50, 50, 100, 100));
+
+            LinearGradientBrush gradient = new LinearGradientBrush(new Rectangle(0, 0, 100, 100),
+                Color.Blue, Color.Yellow, LinearGradientMode.ForwardDiagonal);
+            formGraphics.FillEllipse(gradient, new Rectangle(50, 150, 100, 100));
+
+            //Pacman !!!!
+            Brush myBrush2;
+            myBrush2 = new SolidBrush(Color.Yellow);
+            formGraphics.FillPie(myBrush2, new Rectangle(25, 0, 100, 100), 45, 270);
+            
+            Font drawFont = new Font("Arial", 16);
+            formGraphics.DrawString("PACMAN", drawFont, gradient, 100, 250);
+
+
+            drawFont.Dispose();
+            myBrush2.Dispose();
+            myBrush.Dispose();
+            myPen.Dispose();
+            formGraphics.Dispose();
+        }
+    }
+}
