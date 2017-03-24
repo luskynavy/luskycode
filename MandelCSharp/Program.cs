5d9b7d61346480205c20274f0a147bbad8be0efc
LuskyWork <Lusky@work.com>
2017-03-24 17:42:11 +0100

    MandelCSharp 2017-03-24 updated to vs 2010 and added parallel.for version

--- MandelCSharp/Program.cs
+++ MandelCSharp/Program.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Threading;
+using System.Threading.Tasks;
+
+/***************************************************************
+ * Mandelbrot with 2 methods : double for or for-Parallel.For  *
+****************************************************************/
 namespace MandelCSharp
 {
+    //choose calc type
     using TYPE_CALC = System.Double;
     //using TYPE_CALC = System.Single;
 
     class Program
     {
+        //import time function
         [System.Runtime.InteropServices.DllImport("KERNEL32")]
         private static extern bool QueryPerformanceCounter(ref long lpPerformanceCount);
 
         [System.Runtime.InteropServices.DllImport("KERNEL32")]
         private static extern bool QueryPerformanceFrequency(ref long lpFrequency);
 
-
+        //time function
         public static double CurrentSecond
         {
             get
@@ -29,11 +37,18 @@ namespace MandelCSharp
             }
         }
 
+        /*private static IEnumerable<TYPE_CALC> Iterate(TYPE_CALC fromInclusive, TYPE_CALC toExclusive, TYPE_CALC step)
+        {
+            for (TYPE_CALC d = fromInclusive; d < toExclusive; d += step) yield return d;
+        }*/
+
         static void Main(string[] args)
         {
+            //method with 2 for
+            //start timer
             double starttime = CurrentSecond;
 
-            int steps = 3000;
+            int steps = 4000;
             TYPE_CALC xmin = -2;
             TYPE_CALC ymin = -1;
             TYPE_CALC xmax = 1;
@@ -45,15 +60,19 @@ namespace MandelCSharp
 
             int[] mandel = new int[steps * steps];
 
-            int xmandel = 0;
-            int ymandel = 0;
+            int xmandel = 0;            
             for (TYPE_CALC x = xmin; xmandel < steps; x += xstep, xmandel++)
-            {
-                ymandel = 0;
-                for (TYPE_CALC y = ymin; ymandel < steps; y += ystep, ymandel++)
+            {                
+                //TYPE_CALC y = ymin;
+                int ymandel = 0;
+                for (ymandel = 0; ymandel < steps; ymandel++)
                 {
+                    //compute y from ymandel
+                    TYPE_CALC y = ymin + ymandel * ystep;
+
                     TYPE_CALC x1 = 0;
                     TYPE_CALC y1 = 0;
+                    //max level 100
                     int looper = 0;
                     while (looper < 100 && Math.Sqrt((x1 * x1) + (y1 * y1)) < 2)
                     {
@@ -64,16 +83,83 @@ namespace MandelCSharp
                     }
                     total += looper;
                     mandel[xmandel + ymandel * steps] = looper;
+
+                    //y += ystep;
                 }
             }
 
+            //end timer
             double time = CurrentSecond - starttime;
+
+            //show results
+            Console.WriteLine("double for way:");
 
             Console.WriteLine("mandel  500 200 " + mandel[500 + 200 * steps]);
 
             Console.WriteLine("total " + total);
 
             Console.WriteLine("time " + time);
+
+            Console.WriteLine();
+
+
+            //method with for and Parallel.For
+            //start timer
+            starttime = CurrentSecond;
+
+            total = 0;
+
+            mandel = new int[steps * steps];
+
+            xmandel = 0;
+            for (TYPE_CALC x = xmin; xmandel < steps; x += xstep, xmandel++)
+            {
+                Parallel.For(0, steps, yformandel =>                
+                {
+                    //compute y from yformandel
+                    TYPE_CALC y = ymin + yformandel * ystep;
+
+                    TYPE_CALC x1 = 0;
+                    TYPE_CALC y1 = 0;
+                    //max level 100
+                    int looper = 0;
+                    while (looper < 100 && Math.Sqrt((x1 * x1) + (y1 * y1)) < 2)
+                    {
+                        looper++;
+                        TYPE_CALC xx = (x1 * x1) - (y1 * y1) + x;
+                        y1 = 2 * x1 * y1 + y;
+                        x1 = xx;
+                    }
+                    //equivalent to "total += looper" but with locks between threads for Parallel.For
+                    Interlocked.Add(ref total, looper);
+                    mandel[xmandel + yformandel * steps] = looper;
+                }
+                );
+            }           
+
+            //end timer
+            time = CurrentSecond - starttime;
+
+            //show results
+            Console.WriteLine("for and Parallel.For way:");
+
+            Console.WriteLine("mandel  500 200 " + mandel[500 + 200 * steps]);
+
+            Console.WriteLine("total Interlocked " + total);
+
+            //recompute total from array 
+            total = 0;
+            for (int i = 0; i < mandel.Length; i++)
+            {
+                total += mandel[i];
+            }
+
+            Console.WriteLine("total array " + total);
+
+            Console.WriteLine("time " + time);
+
+            //wait a key
+            Console.ReadKey();
         }
     }
 }
