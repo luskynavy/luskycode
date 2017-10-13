1f1464e5e404dfdeb1d5e7fb282af1e10512f833
LuskyWork <Lusky@work.com>
2017-10-13 16:51:47 +0200

    CourseTRForms 2017-10-13 project added, forms version of CourseTR

---
+++ CourseTRForms/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace CourseTRForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.search = new System.Windows.Forms.Button();
+            this.results = new System.Windows.Forms.TextBox();
+            this.wantedSum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.values = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(197, 121);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(75, 23);
+            this.search.TabIndex = 0;
+            this.search.Text = "Search";
+            this.search.UseVisualStyleBackColor = true;
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // results
+            // 
+            this.results.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.results.Location = new System.Drawing.Point(10, 23);
+            this.results.Multiline = true;
+            this.results.Name = "results";
+            this.results.ReadOnly = true;
+            this.results.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.results.Size = new System.Drawing.Size(264, 124);
+            this.results.TabIndex = 2;
+            // 
+            // wantedSum
+            // 
+            this.wantedSum.Location = new System.Drawing.Point(15, 121);
+            this.wantedSum.Name = "wantedSum";
+            this.wantedSum.Size = new System.Drawing.Size(98, 20);
+            this.wantedSum.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Values";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Wanted sum";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.results);
+            this.groupBox1.Location = new System.Drawing.Point(0, 172);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(10);
+            this.groupBox1.Size = new System.Drawing.Size(284, 157);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Results";
+            // 
+            // values
+            // 
+            this.values.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.values.Location = new System.Drawing.Point(15, 26);
+            this.values.Name = "values";
+            this.values.Size = new System.Drawing.Size(257, 76);
+            this.values.TabIndex = 6;
+            this.values.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 329);
+            this.Controls.Add(this.values);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.wantedSum);
+            this.Controls.Add(this.search);
+            this.Name = "Form1";
+            this.Text = "CourseTR";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button search;
+        private System.Windows.Forms.TextBox results;
+        private System.Windows.Forms.TextBox wantedSum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox values;
+    }
+}
+
---
+++ CourseTRForms/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using NUnit.Framework;
+
+namespace CourseTRForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            values.Text = "2,41; 3,24; 2,09; 2,56; 3,28; 3,88; 1,70; 4,93; 3,30";            
+            wantedSum.Text = (2.41 + 2.09 + 3.3).ToString();
+        }
+
+        string subsetSet(double[] products, double wantedSum)
+        {
+            string res = "";
+
+            for (int testVal = 0; testVal < Math.Pow(2, products.Length); testVal++)
+            {
+                double sum = 0;
+                for (int choice = 0; choice < products.Length; choice++)
+                {
+                    if (((1 << choice) & testVal) == (1 << choice))
+                    {
+                        sum += products[choice];
+                    }
+                }
+
+                //if (sum == 2.41 + 2.09 + 3.3)
+                //if (sum == wantedSumDouble)
+                if (Math.Abs(sum - wantedSum) < 1e-5)
+                {
+                    for (int j = 0; j < products.Length; j++)
+                    {
+                        if (((1 << j) & testVal) == (1 << j))
+                        {
+                            res += " " + products[j];
+                        }
+                    }
+                    res += "\r\n";
+                }
+            }
+
+            return res;
+        }
+
+        private void search_Click(object sender, EventArgs e)
+        {            
+            //double[] products = { 2.41, 3.24, 2.09, 2.56, 3.28, 3.88, 1.70, 4.93, 3.30 };
+            /*product
+            double[] products = new double[];
+
+            int i = 0;
+            foreach(string d in values.Text.Split(';'))
+            {
+                products[i] = double.Parse(d);
+                i++;
+            }*/
+
+            Stopwatch stopWatch = Stopwatch.StartNew();
+
+            double[] products = values.Text.Split(';').Select(Double.Parse).ToArray();
+
+            double wantedSumDouble = double.Parse(wantedSum.Text);
+
+            results.Text = subsetSet(products, wantedSumDouble);
+
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            //TimeSpan ts = stopWatch.Elapsed;
+            long ts = stopWatch.ElapsedMilliseconds;
+
+            // Format and display the TimeSpan value.
+            /*string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);*/
+            string elapsedTime = ((double)ts / 1000).ToString();
+
+            results.Text += "Done for " + products.Length + " values  in " + elapsedTime + " s";
+        }
+
+       /*[Test]
+        public void testSubsetSet()
+        {
+            double[] products = { 2.41, 3.24, 2.09, 2.56, 3.28, 3.88, 1.70, 4.93, 3.30 };
+            string actualResult = subsetSet(products, 10);
+            string expectedResult = "";
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }*/
+    }
+   
+}
