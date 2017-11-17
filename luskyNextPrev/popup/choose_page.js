55cad7f7e9803253144998ff8e9aae575932a755
LuskyWork <Lusky@work.com>
2017-11-17 15:28:41 +0100

    LuskyNextPrev 2017-11-17 added increment by +10, -10 & refactored code

--- luskyNextPrev/popup/choose_page.js
+++ luskyNextPrev/popup/choose_page.js
@@ -3,38 +3,20 @@ document.addEventListener("click", function(e) {
     return;
   }
   
-  if (e.target.textContent == "Next")
-  {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.update(tabs[0].id, {url: getAndIncrementLastNumber(tabs[0].url)});
-    });
-  }
-  else
-  {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.update(tabs[0].id, {url: getAndDecrementLastNumber(tabs[0].url)});
-    });
-  }
+  //convert the increment to integer and use it to increment the last number in url of current tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.update(tabs[0].id, {url: getAndIncrementLastNumber(tabs[0].url, +e.target.textContent)});	
+  });
 });
 
 /**
-* Get and increment the last number of the url string
-* @param {function(string)} str - URL String encoded
+* Get and increment by increment the last number of the url string
+* @param str URL String encoded
+* @param increment integer increment to be added
 */
-function getAndIncrementLastNumber(str) {
-    return str.replace(/(\D*)(\d+)(\D*)$/, function(s, p1, p2, p3) {
-        var l = p2.length, s = '' + (+p2+1), n = l - s.length;
-		return p1 + '0'.repeat(n) + s + p3;
-    });
-}
-
-/**
-* Get and decrement the last number of the url string
-* @param {function(string)} str - URL String encoded
-*/
-function getAndDecrementLastNumber(str) {
-    return str.replace(/(\D*)(\d+)(\D*)$/, function(s, p1, p2, p3) {
-        var l = p2.length, s = '' + (+p2 - 1), n = l - s.length;
-		return p1 + '0'.repeat(n) + s + p3;
-    });
+function getAndIncrementLastNumber(str, increment) {
+  return str.replace(/(\D*)(\d+)(\D*)$/, function(s, p1, p2, p3) {
+    var l = p2.length, s = '' + (+p2+increment), n = l - s.length;
+    return p1 + '0'.repeat(n) + s + p3;
+  });
 }
