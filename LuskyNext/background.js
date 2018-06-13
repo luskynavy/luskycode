545b911aa4a5871bc46993ed5340bbd2523092fa
LuskyWork <Lusky@work.com>
2018-06-13 17:44:46 +0200

    LuskyNext 2018-06-13 project added

---
+++ LuskyNext/background.js
@@ -0,0 +1,78 @@
+browser.browserAction.onClicked.addListener(onclick);
+
+var testUrlReturn;
+
+function onclick() {
+  var holeLimit;
+    
+  chrome.storage.local.get(null,
+    function(object)
+    {
+      holeLimit = object["holeLimit"] || 10;        
+	  console.log("onclick hole limit " + holeLimit);      
+	  onStorageLoaded(holeLimit);
+  });
+}
+
+function onStorageLoaded(holeLimit) {
+  console.log("onStorageLoaded hole limit " + holeLimit);
+  //console.log("onStorageLoaded fast jump " + fastJump);
+  //console.log("onStorageLoaded dataStep " + dataStep);
+  
+  //convert the increment to integer and use it to increment the last number in url of current tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	//console.log("hole limit " + holeLimit);
+	for (i = 1; i <= holeLimit; i++) {	  
+	  var nextUrl = getAndIncrementLastNumber(tabs[0].url, i);
+	  TestUrl(nextUrl);
+	  console.log(nextUrl + ' ' + testUrlReturn);
+	  if (testUrlReturn == 1)
+		break;
+	}
+    chrome.tabs.update(tabs[0].id, {url: nextUrl});	
+  });
+}
+
+function TestUrl(strURL) {
+
+    var req = new XMLHttpRequest();
+    req.open("GET", strURL, false); //false for synchronous, deprecated, should be synchronous
+    req.onreadystatechange=function() {
+        if (req.readyState == 4) {
+            if (req.status == 200)
+            {
+                //console.log("Url found!");
+				testUrlReturn = 1;
+            } else if (req.status == 404) {
+				console.log("URL doesn't exist!")				
+				testUrlReturn = 0;
+			} else {
+				console.log("Error: Status is " + req.status)				
+				testUrlReturn = 0;
+			}
+        }
+    }
+    req.send();
+}
+
+var elts = document.getElementsByClassName('menu-item');
+
+for(var i=0; i<elts.length; i++) {
+  elts[i].addEventListener('click', onclick);
+}
+
+/**
+* Get and increment by increment the last number of the url string
+* @param str URL String encoded
+* @param increment integer increment to be added
+*/
+function getAndIncrementLastNumber(str, increment) {
+  return str.replace(/(\D*)(\d+)(\D*)$/, function(s, p1, p2, p3) {
+    var l = p2.length, s = '' + (+p2+increment), n = l - s.length;
+	if (n < 0)
+	{
+		n = 0;
+	}
+    return p1 + '0'.repeat(n) + s + p3;
+  });
+}
---
+++ LuskyNext/settings/options.js
@@ -0,0 +1,30 @@
+function saveOptions(e) {
+  e.preventDefault();
+  browser.storage.local.set({
+    holeLimit: document.querySelector("#holeLimit").value,
+	fastJump: document.querySelector("#fastJump").value
+  });
+}
+
+function restoreOptions() {
+
+  chrome.storage.local.get(null,
+    function(object) {
+      document.getElementById("holeLimit").value = object["holeLimit"] || 10;        
+      document.getElementById("fastJump").value = object["fastJump"] || 10;
+  });
+  /*function setCurrentChoice(result) {
+    document.querySelector("#holeLimit").value = result.holeLimit || 10;
+	document.querySelector("#holeLimit").value = result.holeLimit || 10;
+  }
+
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+
+  var getting = browser.storage.local.get("holeLimit");
+  getting.then(setCurrentChoice, onError);*/
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
