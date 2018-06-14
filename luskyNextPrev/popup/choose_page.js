ba05db6b0362eeab28882f096083d8b9d45fdd9c
LuskyWork <Lusky@work.com>
2018-06-14 11:21:31 +0200

    LuskyNextPrev 2018-06-14 cleaning

--- luskyNextPrev/popup/choose_page.js
+++ luskyNextPrev/popup/choose_page.js
@@ -9,11 +9,11 @@ function onclick(e) {
   chrome.storage.local.get(null,
     function(object)
     {
-      holeLimit = object["holeLimit"] || 10;      
+      holeLimit = object["holeLimit"] || 10;
       fastJump = object["fastJump"] || 10;
-	  //console.log("onclick hole limit " + holeLimit);
-      //console.log("onclick fast jump " + +fastJump);	  
-	  onStorageLoaded(holeLimit, fastJump, dataStep);
+      //console.log("onclick hole limit " + holeLimit);
+      //console.log("onclick fast jump " + +fastJump);
+      onStorageLoaded(holeLimit, fastJump, dataStep);
   });
 }
 
@@ -24,49 +24,47 @@ function onStorageLoaded(holeLimit, fastJump, dataStep) {
   
   var dataStepSign;
   if (dataStep >= 0) {
-	if (dataStep > 1)
-		dataStep = +fastJump;
-	dataStepSign = 1;
+    if (dataStep > 1)
+      dataStep = +fastJump;
+    dataStepSign = 1;
   } else {
-	if (dataStep < -1)
-	  dataStep = -fastJump;	
-	dataStepSign = -1;
-  }  
+    if (dataStep < -1)
+      dataStep = -fastJump;	
+    dataStepSign = -1;
+  }
 
   //convert the increment to integer and use it to increment the last number in url of current tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	//console.log("hole limit " + holeLimit);
-	for (i = 0; i < holeLimit; i++) {	  
-	  var nextUrl = getAndIncrementLastNumber(tabs[0].url, dataStep + dataStepSign * i);
-	  TestUrl(nextUrl);
-	  console.log(nextUrl + ' ' + testUrlReturn);
-	  if (testUrlReturn == 1)
-		break;
-	}
+    //console.log("hole limit " + holeLimit);
+    for (i = 0; i < holeLimit; i++) {
+      var nextUrl = getAndIncrementLastNumber(tabs[0].url, dataStep + dataStepSign * i);
+      TestUrl(nextUrl);
+      console.log(nextUrl + ' ' + testUrlReturn);
+      if (testUrlReturn == 1)
+        break;
+    }
     chrome.tabs.update(tabs[0].id, {url: nextUrl});	
   });
 }
 
 function TestUrl(strURL) {
-
-    var req = new XMLHttpRequest();
-    req.open("GET", strURL, false); //false for synchronous, deprecated, should be synchronous
-    req.onreadystatechange=function() {
-        if (req.readyState == 4) {
-            if (req.status == 200)
-            {
-                //console.log("Url found!");
-				testUrlReturn = 1;
-            } else if (req.status == 404) {
-				//console.log("URL doesn't exist!")				
-				testUrlReturn = 0;
-			} else {
-				//console.log("Error: Status is " + req.status)				
-				testUrlReturn = 0;
-			}
-        }
+  var req = new XMLHttpRequest();
+  req.open("GET", strURL, false); //false for synchronous, deprecated, should be synchronous
+  req.onreadystatechange=function() {
+    if (req.readyState == 4) {
+      if (req.status == 200) {
+        //console.log("Url found!");
+        testUrlReturn = 1;
+      } else if (req.status == 404) {
+        //console.log("URL doesn't exist!")
+        testUrlReturn = 0;
+      } else {
+        //console.log("Error: Status is " + req.status)
+        testUrlReturn = 0;
+      }
     }
-    req.send();
+  }
+  req.send();
 }
 
 var elts = document.getElementsByClassName('menu-item');
@@ -83,10 +81,10 @@ for(var i=0; i<elts.length; i++) {
 function getAndIncrementLastNumber(str, increment) {
   return str.replace(/(\D*)(\d+)(\D*)$/, function(s, p1, p2, p3) {
     var l = p2.length, s = '' + (+p2+increment), n = l - s.length;
-	if (n < 0)
-	{
-		n = 0;
-	}
+    if (n < 0)
+    {
+        n = 0;
+    }
     return p1 + '0'.repeat(n) + s + p3;
   });
 }
