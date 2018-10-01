7a29c2ac6a59ea80e86de551d1da6b5dab9a26cb
LuskyWork <Lusky@work.com>
2018-10-01 14:36:46 +0200

    SeleniumTest 2018-10-01 updated to 3.14, added wait

--- SeleniumTest/SeleniumTest/Program.cs
+++ SeleniumTest/SeleniumTest/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using OpenQA.Selenium;
 using System.Threading;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumTest
 {
@@ -18,16 +19,25 @@ namespace SeleniumTest
             driver = new OpenQA.Selenium.Firefox.FirefoxDriver();
             //driver = new OpenQA.Selenium.Chrome.ChromeDriver();
 
+            WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 15));
+
 			//open google and do a search
             driver.Navigate().GoToUrl("https://www.google.ch/");
+
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("lst-ib")));
             driver.FindElement(By.Id("lst-ib")).Click();
             driver.FindElement(By.Id("lst-ib")).Clear();
             driver.FindElement(By.Id("lst-ib")).SendKeys("test");
             driver.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
-			//a little wait to be sure the web page is loaded
-            Thread.Sleep(500);
-            driver.FindElement(By.LinkText("Images")).Click();
+
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.LinkText("Vidéos")));
+
+            driver.FindElement(By.LinkText("Vidéos")).Click();
+
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.LinkText("Tous")));
             driver.FindElement(By.LinkText("Tous")).Click();
+
+            Thread.Sleep(5000);
 
 			//close the web driver
             driver.Close();
