067d63690798a3c5baf4d8331d9fcfe372106b08
LuskyHome <Lusky@home.com>
2018-12-10 00:34:11 +0100

    RemoveNonAlpha 2018-12-10 project added

---
+++ removeNonAlpha/removeNonAlpha/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace removeNonAlpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //current dir for searching
+            string bmpDir = ".";
+
+            string[] files = Directory.GetFiles(bmpDir, "*");
+
+            //if found some files
+            if (files.Length > 0)
+            {
+                string fCleaned;
+                foreach (string f in files)
+                {
+                    Regex rgx = new Regex("[^a-zA-Z0-9 -.]");
+                    fCleaned = rgx.Replace(f, "");
+                    fCleaned = fCleaned.Replace("--", "-");
+                    fCleaned = fCleaned.Replace("--", "-");
+                    fCleaned = fCleaned.Replace("--", "-");
+                    Console.WriteLine("copy /b " + f.Substring(2) + " " + fCleaned.Substring(1));
+                }
+            }        
+        }
+    }
+}
