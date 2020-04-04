4e2fa28cbe5530970d96459677556c9eafac7a71
ykalapm <yvan.kalafatov@passman.fr>
2020-04-04 15:58:08 +0200

    BraceMatchingVSIX 2020-04-04 added project : vs 2019 extension matching braces for classic asp

---
+++ BraceMatchingVSIX/BraceMatchingVSIX/BraceMatchingVSIXPackage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace BraceMatchingVSIX
+{
+	/// <summary>
+	/// This is the class that implements the package exposed by this assembly.
+	/// </summary>
+	/// <remarks>
+	/// <para>
+	/// The minimum requirement for a class to be considered a valid package for Visual Studio
+	/// is to implement the IVsPackage interface and register itself with the shell.
+	/// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+	/// to do it: it derives from the Package class that provides the implementation of the
+	/// IVsPackage interface and uses the registration attributes defined in the framework to
+	/// register itself and its components with the shell. These attributes tell the pkgdef creation
+	/// utility what data to put into .pkgdef file.
+	/// </para>
+	/// <para>
+	/// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+	/// </para>
+	/// </remarks>
+	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+	[Guid(BraceMatchingVSIXPackage.PackageGuidString)]
+	//[ProvideLanguageExtension(typeof(AspLanguage), ".asp")] //OK filter only .asp for purple background and if [ContentType("html")] for EditorClassifier1Provider
+	//[ProvideLanguageService(typeof(AspLanguage), "AspLanguage", 0x12345, ShowDropDownOptions = true, MatchBraces = true, MatchBracesAtCaret = true, ShowMatchingBrace = true)]
+	public sealed class BraceMatchingVSIXPackage : AsyncPackage
+	{
+		/// <summary>
+		/// BraceMatchingVSIXPackage GUID string.
+		/// </summary>
+		public const string PackageGuidString = "c87ae807-3ce4-4502-8fc0-8cce1cdd1196";
+
+		#region Package Members
+
+		/// <summary>
+		/// Initialization of the package; this method is called right after the package is sited, so this is the place
+		/// where you can put all the initialization code that rely on services provided by VisualStudio.
+		/// </summary>
+		/// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+		/// <param name="progress">A provider for progress updates.</param>
+		/// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+		{
+			// When initialized asynchronously, the current thread may be a background thread at this point.
+			// Do any initialization that requires the UI thread after switching to the UI thread.
+			await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+		}
+
+		#endregion Package Members
+	}
+}
---
+++ BraceMatchingVSIX/BraceMatchingVSIX/LanguageService/AspLanguage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.Package;
+using Microsoft.VisualStudio.TextManager.Interop;
+using System.Runtime.InteropServices;
+
+namespace BraceMatchingVSIX
+{
+	public class AspLanguage : LanguageService
+	{
+		public AspLanguage()
+		{
+
+		}
+		public override string Name => throw new NotImplementedException();
+
+		public override string GetFormatFilterList()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override LanguagePreferences GetLanguagePreferences()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override IScanner GetScanner(IVsTextLines buffer)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override AuthoringScope ParseSource(ParseRequest req)
+		{
+			throw new NotImplementedException();
+		}
+
+		/*public override TypeAndMemberDropdownBars CreateDropDownHelper(IVsTextView forView)
+		{
+			return new DropDownTocBars(this/*, forView*//*);
+			//return base.CreateDropDownHelper(forView);
+		}*/
+	}
+}
