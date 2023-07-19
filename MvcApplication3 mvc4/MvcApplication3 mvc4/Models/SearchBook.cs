3751add96ebc6d0a6b6bf20f40f47e7263adb59a
ykalapm <yvan.kalafatov@passman.fr>
2023-07-19 17:37:16 +0200

    MvcApplication3 2023-07-19 : added navigation with keys (left, right, home, end, escape)

--- MvcApplication3 mvc4/MvcApplication3 mvc4/Models/SearchBook.cs
+++ MvcApplication3 mvc4/MvcApplication3 mvc4/Models/SearchBook.cs
@@ -8,8 +8,13 @@ namespace MvcApplication3_mvc4.Models
 {
     public class SearchBook
     {
+        //Current name filter
         public string NameSearch { get; set; }
+
+        //Current Page number
         public int Page { get; set; }
+
+		//List of books
         //public List<Book> BooksList { get; set; }
         public IPagedList<Book> BooksList { get; set; }
     }
