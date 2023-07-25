a46d83c0037ecd092a26a52561c689b76a8dde54
Lusky <lusky@work>
2023-07-25 10:15:49 +0200

    OnlyAscii : 2023-07-25 : cleaning

--- OnlyAscii/OnlyAscii/Program.cs
+++ OnlyAscii/OnlyAscii/Program.cs
@@ -1,47 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace OnlyAscii
 {
-	internal class Program
-	{
-		private static void Main(string[] args)
-		{
-			string pwd = Path.GetFileName(Environment.CurrentDirectory);
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string pwd = Path.GetFileName(Environment.CurrentDirectory);
 
-			DirectoryInfo d = new DirectoryInfo(@"."); //in current folder
+            DirectoryInfo d = new DirectoryInfo(@"."); //in current folder
 
-			//order descending by name length to avoid name override
-			//so shorter name will not override longer name starting with folder
-			IOrderedEnumerable<FileInfo> orderedEnumerable = d.GetFiles("*.*").OrderByDescending(f => f.Name.Length);
-			//FileInfo[] Files = orderedEnumerable; //Getting files
+            //order descending by name length to avoid name override
+            //so shorter name will not override longer name starting with folder
+            IOrderedEnumerable<FileInfo> orderedEnumerable = d.GetFiles("*.*").OrderByDescending(f => f.Name.Length);
+            //FileInfo[] Files = orderedEnumerable; //Getting files
 
-			foreach (FileInfo file in orderedEnumerable)
-			{
-				string newName = Regex.Replace(file.Name, @"[^\u0000-\u007F]+", string.Empty);
+            foreach (FileInfo file in orderedEnumerable)
+            {
+                string newName = Regex.Replace(file.Name, @"[^\u0000-\u007F]+", string.Empty);
 
-				//only if newname is different
-				if (file.Name != newName)
-				{
-					Console.WriteLine(file.Name + " => " + newName);
-					//remove non ascii chars
-					try
-					{
+                //only if newname is different
+                if (file.Name != newName)
+                {
+                    Console.WriteLine(file.Name + " => " + newName);
+                    //remove non ascii chars
+                    try
+                    {
                         System.IO.File.Move(file.Name, newName);
                     }
-					catch (Exception ex)
-					{
-						Console.WriteLine(ex.ToString());
-						continue;
-					}
-
-				}
-			}
-		}
-	}
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        continue;
+                    }
+                }
+            }
+        }
+    }
 }
