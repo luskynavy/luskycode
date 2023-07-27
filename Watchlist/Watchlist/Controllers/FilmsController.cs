e5d4abcc840eef0325d86f5d2ed0b700baa45a3b
Lusky <lusky@home>
2023-07-27 16:05:38 +0200

    Watchlist : 2023-07-27 : possibility to add or remove movie to list

--- Watchlist/Watchlist/Controllers/FilmsController.cs
+++ Watchlist/Watchlist/Controllers/FilmsController.cs
@@ -6,24 +6,115 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Watchlist.Data;
+using Watchlist.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace Watchlist.Controllers
 {
+    [Authorize]
     public class FilmsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<Utilisateur> _gestionnaire;
 
-        public FilmsController(ApplicationDbContext context)
+        public FilmsController(ApplicationDbContext contexte,
+           UserManager<Utilisateur> gestionnaire)
         {
-            _context = context;
+            _context = contexte;
+            _gestionnaire = gestionnaire;
         }
+
+        [HttpGet]
+        public async Task<string> RecupererIdUtilisateurCourant()
+        {
+            Utilisateur utilisateur = await GetCurrentUserAsync();
+            return utilisateur?.Id;
+        }
+
+        private Task<Utilisateur> GetCurrentUserAsync() =>
+        _gestionnaire.GetUserAsync(HttpContext.User);
 
         // GET: Films
         public async Task<IActionResult> Index()
         {
-            return _context.Films != null ?
-                        View(await _context.Films.ToListAsync()) :
-                        Problem("Entity set 'ApplicationDbContext.Films'  is null.");
+            if (_context.Films != null)
+            {
+                var idUtilisateur = await RecupererIdUtilisateurCourant();
+                var modele = await _context.Films.Select(x =>
+                        new ModeleVueFilm
+                        {
+                            IdFilm = x.Id,
+                            Titre = x.Titre,
+                            Annee = x.Annee
+                        }).ToListAsync();
+                foreach (var item in modele)
+                {
+                    var m = await _context.FilmsUtilisateur.FirstOrDefaultAsync(x =>
+                               x.IdUtilisateur == idUtilisateur && x.IdFilm == item.IdFilm);
+                    if (m != null)
+                    {
+                        item.PresentDansListe = true;
+                        item.Note = m.Note;
+                        item.Vu = m.Vu;
+                    }
+                }
+                return View(modele);
+            }
+            else
+            {
+                return Problem("Entity set 'ApplicationDbContext.Films'  is null.");
+            }
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> AjouterSupprimer(int id, int val)
+        {
+            int valret = -1;
+
+            var utilisateur = await GetCurrentUserAsync();
+            var idUtilisateur = utilisateur.Id;
+
+            if (val == 1)
+            {
+                // s'il existe un enregistrement dans FilmsUtilisateur qui contient à la fois l'identifiant de l'utilisateur
+                // et celui du film, alors le film existe dans la liste de films et peut
+                // être supprimé
+                var film = _context.FilmsUtilisateur.FirstOrDefault(x =>
+                        x.IdFilm == id && x.IdUtilisateur == idUtilisateur);
+                if (film != null)
+                {
+                    _context.FilmsUtilisateur.Remove(film);
+                    valret = 0;
+                }
+
+            }
+            else
+            {
+                var film = _context.Films.FirstOrDefault(x => x.Id == id );
+                if (film != null)
+                {
+                    // le film n'est pas dans la liste de films, nous devons donc
+                    // créer un nouvel objet FilmUtilisateur et l'ajouter à la base de données.
+                    _context.FilmsUtilisateur.Add(
+                       new FilmUtilisateur
+                       {
+                           IdUtilisateur = idUtilisateur,
+                           IdFilm = id,
+                           Vu = false,
+                           Note = 0,
+                           User = utilisateur,
+                           Film = film
+                       }
+                    );
+                    valret = 1;
+                }
+            }
+            // nous pouvons maintenant enregistrer les changements dans la base de données
+            await _context.SaveChangesAsync();
+            // et renvoyer notre valeur de retour (-1, 0 ou 1) au script qui a appelé
+            // cette méthode depuis la page Index
+            return Json(valret);
         }
 
         // GET: Films/Details/5
