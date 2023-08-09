8084e5ea784498e2f4530bb935236af0aafc6bcd
Lusky <lusky@work>
2023-08-09 13:37:47 +0200

    SqliteCoreConsole 2023-08-09 : updated to net7.0

--- SqliteCoreConsole/SqliteCoreConsole/Program.cs
+++ SqliteCoreConsole/SqliteCoreConsole/Program.cs
@@ -3,44 +3,50 @@ using System.Linq;
 
 namespace SqliteCoreConsole
 {
-	internal class Program
-	{
-		private static void Main(string[] args)
-		{
-			var folder = Environment.CurrentDirectory;
-			using var db = new BloggingContext();
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var folder = Environment.CurrentDirectory;
+            using var db = new BloggingContext();
 
-			//Pour créér la bdd, sur la Package Manager Console
-			//Add-Migration InitialCreate
-			//Update - Database
+            //Pour créér la bdd, sur la Package Manager Console
+            //Add-Migration InitialCreate (pas obligatoire car la migration n'a rien fait de plus ?)
+            //Update-Database
 
-			// Note: This sample requires the database to be created before running.
-			//Construit le chemin du projet en remontant \bin\Debug\net5.0
-			db.DbPath = System.IO.Path.Join(folder + "\\..\\..\\..\\", "blogging.db");
-			Console.WriteLine($"Database path: {db.DbPath}.");
+            // Note: This sample requires the database to be created before running.
+            //Construit le chemin du projet en remontant \bin\Debug\netX.0
+            db.DbPath = System.IO.Path.Join(folder + "\\..\\..\\..\\", "blogging.db");
+            Console.WriteLine($"Database path: {db.DbPath}.");
 
-			// Create
-			Console.WriteLine("Inserting a new blog");
-			db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-			db.SaveChanges();
+            Console.WriteLine("Blog count : " + db.Blogs.Count());
 
-			// Read
-			Console.WriteLine("Querying for a blog");
-			var blog = db.Blogs
-				.OrderBy(b => b.BlogId)
-				.First();
+            // Create
+            Console.WriteLine("Inserting a new blog");
+            db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+            db.SaveChanges();
 
-			// Update
-			Console.WriteLine("Updating the blog and adding a post");
-			blog.Url = "https://devblogs.microsoft.com/dotnet";
-			blog.Posts.Add(
-				new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
-			db.SaveChanges();
+            Console.WriteLine("Blog count : " + db.Blogs.Count());
 
-			// Delete
-			Console.WriteLine("Delete the blog");
-			db.Remove(blog);
-			db.SaveChanges();
-		}
-	}
+            // Read
+            Console.WriteLine("Querying for a blog");
+            var blog = db.Blogs
+                .OrderBy(b => b.BlogId)
+                .First();
+
+            // Update
+            Console.WriteLine("Updating the blog and adding a post");
+            blog.Url = "https://devblogs.microsoft.com/dotnet";
+            blog.Posts.Add(
+                new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+            db.SaveChanges();
+
+            // Delete
+            Console.WriteLine("Delete the blog");
+            db.Remove(blog);
+            db.SaveChanges();
+
+            Console.WriteLine("Blog count : " + db.Blogs.Count());
+        }
+    }
 }
