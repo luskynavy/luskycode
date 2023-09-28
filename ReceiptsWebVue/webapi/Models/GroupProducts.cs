04fe53fd75da70bca85a097ebcd0935c731b82b2
Lusky <lusky@work>
2023-09-28 16:46:57 +0200

    ReceiptsWebVue 2023-09-28: Products and GroupProducts api

---
+++ ReceiptsWebVue/webapi/Models/GroupProducts.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReceiptsWeb.Models
+{
+	public class GroupProducts
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Group { get; set; }
+
+		[Precision(18, 2)]
+		public decimal Min { get; set; }
+
+		[Precision(18, 2)]
+		public decimal Max { get; set; }
+
+		public DateTime MinDate { get; set; }
+
+		public DateTime MaxDate { get; set; }
+
+		[Precision(18, 2)]
+		public decimal PriceRatio { get; set; }
+
+		public int PricesCount { get; set; }
+	}
+}
--- ReceiptsWebVue/webapi/Program.cs
+++ ReceiptsWebVue/webapi/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using webapi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//Ajoute la bdd à l'injection de dépendance
+builder.Services.AddDbContext<ReceiptsContext>(options =>
+	options.UseSqlServer(connectionString));
 
 // Add services to the container.
 
