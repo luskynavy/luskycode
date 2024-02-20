4cb48d59bb1de3e0c7301e2089ca494d0be72b4e
Lusky <lusky@work>
2024-02-20 13:10:10 +0100

    ReceiptsWinFormsBlazor 2024-02-02 Added project Blazor Hybrid WinForms version of ReceiptsWebBlazor

---
+++ ReceiptsWinFormsBlazor/ReceiptsWinFormsBlazor/Form1.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.Metrics;
+using ReceiptsBlazorWinForms.Components;
+using ReceiptsBlazorWinForms.Models;
+
+namespace ReceiptsBlazorWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var services = new ServiceCollection();
+
+            //Get the connectionString from appsettings.json
+            //Must install nuget Microsoft.Extensions.Configuration & Microsoft.Extensions.Configuration.Json
+            IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile(@"appsettings.json", false, true);
+            IConfigurationRoot configuration = builder.Build();
+            string? connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContextFactory<ReceiptsContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddWindowsFormsBlazorWebView();
+            blazorWebView1.HostPage = "wwwroot\\index.html";
+            blazorWebView1.Services = services.BuildServiceProvider();
+
+            blazorWebView1.RootComponents.Add<App>("#app");
+        }
+    }
+}
---
+++ ReceiptsWinFormsBlazor/ReceiptsWinFormsBlazor/Program.cs
@@ -0,0 +1,23 @@
+using System.Windows.Forms;
+using ReceiptsBlazorWinForms.Models;
+
+namespace ReceiptsBlazorWinForms
+{
+    public static class Program
+    {
+        public static Form1 MyForm { get; set; }
+
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            MyForm = new Form1();
+            Application.Run(MyForm);
+        }
+    }
+}
