e213d14f17a708566806bf8981d9cd1dfbf6887a
Lusky <lusky@work>
2024-03-07 13:53:26 +0100

    ReceiptsWebBlazor 2024-03-07 Group Products class

---
+++ ReceiptsWebBlazor/ReceiptsWebBlazor/Models/GroupProduct.cs
@@ -0,0 +1,34 @@
+﻿#nullable disable
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Resources;
+
+namespace ReceiptsWebBlazor.Models;
+
+public class GroupProduct
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Group { get; set; }
+
+    [Precision(18, 2)]
+    public decimal Min { get; set; }
+
+    [Precision(18, 2)]
+    public decimal Max { get; set; }
+
+    public DateTime MinDate { get; set; }
+
+    public DateTime MaxDate { get; set; }
+
+    [Precision(18, 2)]
+    public decimal PriceRatio { get; set; }
+
+    public int PricesCount { get; set; }
+}
