ba0538d936e65deb0f44cb24df79d0083b29f761
Lusky <lusky@home>
2024-04-11 17:59:39 +0200

    ReceiptsWinFormsBlazor 2024-04-11 Missing class

---
+++ ReceiptsWinFormsBlazor/ReceiptsWinFormsBlazor/Models/GroupProduct.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReceiptsBlazorWinForms.Models;
+
+public class GroupProduct
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Group { get; set; }
+
+    [Precision(18, 2)]
+    public decimal Min { get; set; }
+
+    [Precision(18, 2)]
+    public decimal Max { get; set; }
+
+    public DateTime MinDate { get; set; }
+
+    public DateTime MaxDate { get; set; }
+
+    [Precision(18, 2)]
+    public decimal PriceRatio { get; set; }
+
+    public int PricesCount { get; set; }
+}
