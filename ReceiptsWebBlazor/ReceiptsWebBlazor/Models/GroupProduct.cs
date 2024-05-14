048983c83fc8958c057411b98d3da42ab1ecfe84
Lusky <lusky@work>
2024-05-14 16:15:00 +0200

    ReceiptsWebBlazor 2024-05-14 Added last price, previous price and tendency

--- ReceiptsWebBlazor/ReceiptsWebBlazor/Models/GroupProduct.cs
+++ ReceiptsWebBlazor/ReceiptsWebBlazor/Models/GroupProduct.cs
@@ -5,25 +5,33 @@ namespace ReceiptsWebBlazor.Models;
 
 public class GroupProduct
 {
-    [Key]
-    public int Id { get; set; }
+	[Key]
+	public int Id { get; set; }
 
-    public string? Name { get; set; }
+	public string? Name { get; set; }
 
-    public string? Group { get; set; }
+	public string? Group { get; set; }
 
-    [Precision(18, 2)]
-    public decimal Min { get; set; }
+	[Precision(18, 2)]
+	public decimal Min { get; set; }
 
-    [Precision(18, 2)]
-    public decimal Max { get; set; }
+	[Precision(18, 2)]
+	public decimal Max { get; set; }
 
-    public DateTime MinDate { get; set; }
+	[Precision(18, 2)]
+	public decimal PreviousPrice { get; set; }
 
-    public DateTime MaxDate { get; set; }
+	[Precision(18, 2)]
+	public decimal LastPrice { get; set; }
 
-    [Precision(18, 2)]
-    public decimal PriceRatio { get; set; }
+	public DateTime MinDate { get; set; }
 
-    public int PricesCount { get; set; }
+	public DateTime MaxDate { get; set; }
+
+	[Precision(18, 2)]
+	public decimal PriceRatio { get; set; }
+
+	public int PricesCount { get; set; }
+
+	public IEnumerable<decimal> PricesList { get; set; }
 }
