59ec5cf7e33ffd448f0fc1df6011bd81b10c2147
Lusky <lusky@home>
2024-05-31 20:44:24 +0200

    SortImagesByRatioCore 2024-05-31 Use SortImagesByRatio updated to .net core 8

---
+++ SortImagesByRatioCore/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace SortImagesByRatioCore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.imagesPath = new System.Windows.Forms.TextBox();
+            this.buttonSelectPath = new System.Windows.Forms.Button();
+            this.richTextBoxResultsH = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxResultsW = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.desiredWidth = new System.Windows.Forms.NumericUpDown();
+            this.desiredHeight = new System.Windows.Forms.NumericUpDown();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.desiredWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.desiredHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Sort jpeg";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "desired width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "desired height";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "images path";
+            // 
+            // imagesPath
+            // 
+            this.imagesPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.imagesPath.Location = new System.Drawing.Point(101, 68);
+            this.imagesPath.Name = "imagesPath";
+            this.imagesPath.Size = new System.Drawing.Size(476, 20);
+            this.imagesPath.TabIndex = 3;
+            // 
+            // buttonSelectPath
+            // 
+            this.buttonSelectPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSelectPath.Location = new System.Drawing.Point(583, 66);
+            this.buttonSelectPath.Name = "buttonSelectPath";
+            this.buttonSelectPath.Size = new System.Drawing.Size(24, 23);
+            this.buttonSelectPath.TabIndex = 4;
+            this.buttonSelectPath.Text = "...";
+            this.buttonSelectPath.UseVisualStyleBackColor = true;
+            this.buttonSelectPath.Click += new System.EventHandler(this.buttonSelectPath_Click);
+            // 
+            // richTextBoxResultsH
+            // 
+            this.richTextBoxResultsH.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxResultsH.Location = new System.Drawing.Point(27, 3);
+            this.richTextBoxResultsH.Name = "richTextBoxResultsH";
+            this.richTextBoxResultsH.ReadOnly = true;
+            this.richTextBoxResultsH.Size = new System.Drawing.Size(577, 141);
+            this.richTextBoxResultsH.TabIndex = 6;
+            this.richTextBoxResultsH.Text = "";
+            // 
+            // richTextBoxResultsW
+            // 
+            this.richTextBoxResultsW.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxResultsW.Location = new System.Drawing.Point(27, 3);
+            this.richTextBoxResultsW.Name = "richTextBoxResultsW";
+            this.richTextBoxResultsW.ReadOnly = true;
+            this.richTextBoxResultsW.Size = new System.Drawing.Size(577, 144);
+            this.richTextBoxResultsW.TabIndex = 5;
+            this.richTextBoxResultsW.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "W";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "H";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // desiredWidth
+            // 
+            this.desiredWidth.Location = new System.Drawing.Point(101, 16);
+            this.desiredWidth.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.desiredWidth.Name = "desiredWidth";
+            this.desiredWidth.Size = new System.Drawing.Size(72, 20);
+            this.desiredWidth.TabIndex = 1;
+            // 
+            // desiredHeight
+            // 
+            this.desiredHeight.Location = new System.Drawing.Point(101, 42);
+            this.desiredHeight.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.desiredHeight.Name = "desiredHeight";
+            this.desiredHeight.Size = new System.Drawing.Size(72, 20);
+            this.desiredHeight.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(3, 120);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBoxResultsW);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBoxResultsH);
+            this.splitContainer1.Panel2.Controls.Add(this.label5);
+            this.splitContainer1.Size = new System.Drawing.Size(604, 301);
+            this.splitContainer1.SplitterDistance = 150;
+            this.splitContainer1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 433);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.desiredHeight);
+            this.Controls.Add(this.desiredWidth);
+            this.Controls.Add(this.buttonSelectPath);
+            this.Controls.Add(this.imagesPath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SortImagesByRatio";
+            ((System.ComponentModel.ISupportInitialize)(this.desiredWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.desiredHeight)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox imagesPath;
+        private System.Windows.Forms.Button buttonSelectPath;
+        private System.Windows.Forms.RichTextBox richTextBoxResultsH;
+        private System.Windows.Forms.RichTextBox richTextBoxResultsW;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown desiredWidth;
+        private System.Windows.Forms.NumericUpDown desiredHeight;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ SortImagesByRatioCore/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+using System.IO;
+using ExifLib;
+
+//using System.Windows.Media.Imaging.BitmapDecoder;
+
+/***************************************************************************************
+ * Determine if jpeg are best scaled in portrait or landscape (with 90 degree rotation)
+ * desired width, size and path are parametrables
+***************************************************************************************/
+
+namespace SortImagesByRatioCore
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			//set label in vertical
+			label4.Text = "N\no\nR\no\nt\na\nt\ne";
+			label5.Text = "R\no\nt\na\nt\ne";
+
+			//set default values
+			desiredWidth.Value = 720;
+			desiredHeight.Value = 1520;
+			imagesPath.Text = @"D:\d\.wp\wp";
+		}
+
+		//resize from width * height to wantedWidth * wantedHeight keeping ratio
+		private Size ResizeTo(int width, int height, int wantedWidth, int wantedHeight)
+		{
+			double scaledWidth, scaledHeight;
+			Size wanted = new Size();
+			//compute ratio
+			double ratio = (double)width / height;
+
+			//compute scaled sizes
+			scaledHeight = (double)wantedWidth / ratio;
+			scaledWidth = (double)wantedHeight * ratio;
+
+			//if scaling in height is too big
+			if (scaledHeight > wantedHeight)
+			{
+				wanted.Width = (int)scaledWidth;
+				wanted.Height = wantedHeight;
+			}
+			else
+			{
+				wanted.Width = wantedWidth;
+				wanted.Height = (int)scaledHeight;
+			}
+
+			return wanted;
+		}
+
+        /// <summary>
+        /// Get height and width with ExifReader
+        /// </summary>
+        /// <param name="f"></param>
+        /// <param name="height"></param>
+        /// <param name="width"></param>
+        private static void GetExifReaderHeightWidth(FileInfo f, out int height, out int width)
+        {
+            //open file in readonly non locking file
+            var stream = f.Open(FileMode.Open, FileAccess.Read, FileShare.Read);
+            var exif = ExifReader.ReadJpeg(stream);
+            height = exif.Height;
+            width = exif.Width;
+
+            stream.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+		{
+			//get files list
+			DirectoryInfo dir = new DirectoryInfo(imagesPath.Text);
+			//FileInfo[] files = dir.GetFiles("*.jpg");
+			IEnumerable<FileInfo> files = dir.GetFiles("*.jpeg").Union(dir.GetFiles("*.jpg")).Union(dir.GetFiles("*.png")).Union(dir.GetFiles("*.gif")).Union(dir.GetFiles("*.bmp"));
+
+			//clear results
+			richTextBoxResultsH.Clear();
+			richTextBoxResultsW.Clear();
+			//int scaledWidth, scaledHeight;
+
+			//for each file
+			foreach (FileInfo f in files)
+			//foreach (string f in files)
+			{
+				try
+				{
+					//read the whole image to get the size, bad
+					//Bitmap im = new Bitmap(f.FullName);
+
+					Size im = new Size();
+
+                    //ExifReader, faster than reading whole image but errors with some jpg
+                    /*int height = 0, width = 0;
+					GetExifReaderHeightWidth(f, out height, out width);
+					im.Height = height;
+					im.Width = width;*/
+
+					//read only the header, no additionnal reference, faster
+                    using (FileStream file = new FileStream(f.FullName, FileMode.Open, FileAccess.Read))
+					{
+						using (Image tif = Image.FromStream(stream: file,
+															useEmbeddedColorManagement: false,
+															validateImageData: false))
+						{
+							im.Width = (int)tif.PhysicalDimension.Width;
+							im.Height = (int)tif.PhysicalDimension.Height;
+						}
+					}
+
+					//read only the header, require additionnal wpf reference
+					/*using (var imageStream = File.OpenRead(f.FullName))
+					{
+						var decoder = BitmapDecoder.Create(imageStream, BitmapCreateOptions.IgnoreColorProfile,
+							BitmapCacheOption.Default);
+						im.height = decoder.Frames[0].PixelHeight;
+						im.width = decoder.Frames[0].PixelWidth;
+					}*/
+
+					//compute scaled size
+					Size noRotate = ResizeTo(im.Width, im.Height, (int)desiredWidth.Value, (int)desiredHeight.Value);
+					Size rotate = ResizeTo(im.Height, im.Width, (int)desiredWidth.Value, (int)desiredHeight.Value);
+
+					/*scaledWidth = im.Width * (int)desiredHeight.Value / im.Height;
+					scaledHeight = im.Height * (int)desiredWidth.Value / im.Width;
+					*/
+					//if scaled image don't fit in width, put in height result rich text box
+					//if (scaledWidth > desiredWidth.Value)
+
+					//if scaled image has more surface with no rotation
+					if (noRotate.Width * noRotate.Height >= rotate.Width * rotate.Height)
+					{
+						richTextBoxResultsW.Text += f.Name //+ " " + im.Width + " x " + im.Height + " " + desiredWidth.Text + " x " + scaledHeight + " " + scaledWidth + " x " + desiredHeight.Text
+							+ "\r\n";
+					}
+					//else in height result rich text box
+					else
+					{
+						richTextBoxResultsH.Text += f.Name //+ " " + im.Width + " x " + im.Height + " " + desiredWidth.Text + " x " + scaledHeight + " " + scaledWidth + " x " + desiredHeight.Text
+							+ "\r\n";
+					}
+				}
+				//hum, continue ?
+				catch
+				{
+				}
+			}
+		}
+
+		//show the folder selector for image path
+		private void buttonSelectPath_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+			folderBrowserDialog.SelectedPath = imagesPath.Text;
+
+			DialogResult result = folderBrowserDialog.ShowDialog();
+
+			if (result == DialogResult.OK && !string.IsNullOrEmpty(folderBrowserDialog.SelectedPath))
+			{
+				imagesPath.Text = folderBrowserDialog.SelectedPath;
+			}
+		}
+	}
+}
