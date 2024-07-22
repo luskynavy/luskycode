8279e6d31edc8293e2da648e6c36ae8cd26c4a3d
Lusky <lusky@home>
2024-07-23 01:56:56 +0200

    LuskyPrev 2024-07-23 No reload if no number in url & Fill with zeroes or not left of number

--- LuskyPrev/background.js
+++ LuskyPrev/background.js
@@ -20,18 +20,20 @@ function onStorageLoaded(holeLimit) {
     //console.log("onStorageLoaded fast jump " + fastJump);
     //console.log("onStorageLoaded dataStep " + dataStep);
 
-    //convert the increment to integer and use it to increment the last number in url of current tab
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    //console.log("hole limit " + holeLimit);
-    for (i = 1; i <= holeLimit; i++) {	  
-        var nextUrl = getAndIncrementLastNumber(tabs[0].url, -i);
-        TestUrl(nextUrl);
-        //console.log(nextUrl + ' ' + testUrlReturn);
-        if (testUrlReturn == 1)
-            break;
-    }
-    chrome.tabs.update(tabs[0].id, {url: nextUrl});	
-    });
+    //convert the increment to integer and use it to increment the last number in url of current tab	
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		if (hasNumber(tabs[0].url)) {
+			//console.log("hole limit " + holeLimit);
+			for (i = 1; i <= holeLimit; i++) {	  
+				var nextUrl = getAndIncrementLastNumber(tabs[0].url, -i, true);
+				TestUrl(nextUrl);
+				//console.log(nextUrl + ' ' + testUrlReturn);
+				if (testUrlReturn == 1)
+					break;
+			}
+			chrome.tabs.update(tabs[0].id, {url: nextUrl});	
+		}
+	});
 }
 
 function TestUrl(strURL) {
@@ -62,18 +64,29 @@ for(var i=0; i<elts.length; i++) {
     elts[i].addEventListener('click', onclick);
 }
 
+function hasNumber(str) {
+	return str.match(/(\D*)(\d+)(\D*)$/) != null
+}
+
 /**
 * Get and increment by increment the last number of the url string
 * @param str URL String encoded
 * @param increment integer increment to be added
+* @param fillWithZeroes true if must be filled with zeroes at left
 */
-function getAndIncrementLastNumber(str, increment) {
+function getAndIncrementLastNumber(str, increment, fillWithZeroes) {
     return str.replace(/(\D*)(\d+)(\D*)$/, function(s, p1, p2, p3) {
-        var l = p2.length, s = '' + (+p2+increment), n = l - s.length;
-        if (n < 0)
-        {
-            n = 0;
+    	var l = p2.length, s = '' + (+p2+increment);
+      	if (fillWithZeroes == true) {
+           	var n = l - s.length
+            if (n < 0)
+            {
+                n = 0;
+            }
+          	return p1 + '0'.repeat(n) + s + p3;
         }
-        return p1 + '0'.repeat(n) + s + p3;
+      	else {
+          return p1 + s + p3;
+        }
     });
 }
