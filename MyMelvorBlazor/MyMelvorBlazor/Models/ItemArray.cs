56fc32956664a3f7dd8a06f643d7ba32af560bb2
Lusky <lusky@work>
2024-08-01 10:33:32 +0200

    MyMelvorBlazor 2024-08-01 Selling items & Formatting

--- MyMelvorBlazor/MyMelvorBlazor/Models/ItemArray.cs
+++ MyMelvorBlazor/MyMelvorBlazor/Models/ItemArray.cs
@@ -3,13 +3,13 @@
 	public class ItemArray
 	{
 		public static ItemClass[] Items { get; } =
-		{
+		[
 			new ItemClass(ItemId.Wood, "Wood", "A wood log"),
 			new ItemClass(ItemId.Teak, "Teak", "A teak log"),
 			new ItemClass(ItemId.Fish, "Fish", "A fish"),
 			new ItemClass(ItemId.Catfish, "Catfish", "A catfish"),
 			new ItemClass(ItemId.RawFish, "Raw fish", "A raw fish"),
 			new ItemClass(ItemId.RawCatfish, "Raw catfish", "A raw catfish")
-		};
+		];
 	}
 }
--- MyMelvorBlazor/MyMelvorBlazor/Models/Player.cs
+++ MyMelvorBlazor/MyMelvorBlazor/Models/Player.cs
@@ -1,77 +1,89 @@
 ﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel;
 
 namespace MyMelvorBlazor.Models
 {
-    public static class Player
-    {
-        public static int Xp { get; set; } = 0;
-        public static int Money { get; set; } = 0;
-        public static int CookingLevel { get; set; } = 0;
-        public static int FishingLevel { get; set; } = 0;
-        public static int WoodcuttingLevel { get; set; } = 0;
-        public static List<InventoryItemClass> Inventory { get; set; } = new List<InventoryItemClass>();
+	public static class Player
+	{
+		public static int Xp { get; set; } = 0;
+		public static int Money { get; set; } = 0;
+		public static int CookingLevel { get; set; } = 0;
+		public static int FishingLevel { get; set; } = 0;
+		public static int WoodcuttingLevel { get; set; } = 0;
+		public static List<InventoryItemClass> Inventory { get; set; } = new List<InventoryItemClass>();
 
-        public static void LoadValues()
-        {
-            Xp = 170;
-            Money = 5000;
-            CookingLevel = 5;
-            FishingLevel = 4;
-            WoodcuttingLevel = 3;
-            Inventory = new List<InventoryItemClass> {
-                new InventoryItemClass(ItemId.Wood, 1),
-                new InventoryItemClass(ItemId.RawFish, 1),
-                new InventoryItemClass(ItemId.Teak, 10),
-                new InventoryItemClass(ItemId.Catfish, 2)
-            };
-        }
+		public static void LoadValues()
+		{
+			Xp = 170;
+			Money = 5000;
+			CookingLevel = 5;
+			FishingLevel = 4;
+			WoodcuttingLevel = 3;
+			Inventory = new List<InventoryItemClass> {
+				new(ItemId.Wood, 1),
+				new(ItemId.RawFish, 1),
+				new(ItemId.Teak, 10),
+				new(ItemId.Catfish, 2)
+			};
+		}
 
-        //Add nb 'count' item of id 'idItem'
-        public static void AddToInventory(ItemId idItem, int count)
-        {
-            //Search the item in inventory
-            var foundInventory = Inventory.FirstOrDefault(i => i.Id == idItem);
+		//Add nb 'count' item of id 'idItem'
+		public static void AddToInventory(ItemId idItem, int count)
+		{
+			//Search the item in inventory
+			var foundInventory = Inventory.FirstOrDefault(i => i.Id == idItem);
 
-            //Create the item if not found
-            if (foundInventory == null)
-            {
-                var foundItem = ItemArray.Items.First(i => i.Id == idItem);
-                if (foundItem != null)
-                {
-                    Inventory.Add(new InventoryItemClass(idItem, count));
-                }
-            }
-            //Update item count if found
-            else
-            {
-                foundInventory.Count += count;
+			//Create the item if not found
+			if (foundInventory == null)
+			{
+				var foundItem = ItemArray.Items.First(i => i.Id == idItem);
+				if (foundItem != null)
+				{
+					Inventory.Add(new InventoryItemClass(idItem, count));
+				}
+			}
+			//Update item count if found
+			else
+			{
+				foundInventory.Count += count;
 
-                //Remove item if quantity <= 0
-                if (foundInventory.Count <= 0)
-                {
-                    Inventory.Remove(foundInventory);
-                }
-            }
-        }
+				//Remove item if quantity <= 0
+				if (foundInventory.Count <= 0)
+				{
+					Inventory.Remove(foundInventory);
+				}
+			}
+		}
 
-        //Get item count in inventory
-        public static int GetNbItemInInventory(ItemId idItem)
-        {
-            var foundInventory = Inventory.FirstOrDefault(i => i.Id == idItem);
-            if (foundInventory == null)
-            {
-                return 0;
-            }
-            else
-            {
-                return foundInventory.Count;
-            }
-        }
+		//Get item count in inventory
+		public static int GetNbItemInInventory(ItemId idItem)
+		{
+			var foundInventory = Inventory.FirstOrDefault(i => i.Id == idItem);
+			if (foundInventory == null)
+			{
+				return 0;
+			}
+			else
+			{
+				return foundInventory.Count;
+			}
+		}
 
-        //True if has item in inventory
-        public static bool HasItemInInventory(ItemId idItem)
-        {
-            return Inventory.FirstOrDefault(i => i.Id == idItem) != null;
-        }
-    }
+		//True if has item in inventory
+		public static bool HasItemInInventory(ItemId idItem)
+		{
+			return Inventory.FirstOrDefault(i => i.Id == idItem) != null;
+		}
+
+		//Sell nb 'count' item of id 'idItem'
+		public static void SellItem(ItemId? idItem, int count)
+		{
+			if (idItem != null)
+			{
+				AddToInventory((ItemId)idItem, -count);
+				Money += count * 100;
+			}
+		}
+	}
 }
