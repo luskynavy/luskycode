433c58cc23c26e7c7b1cd07375816ab86651349b
Lusky <lusky@work>
2024-08-01 11:23:18 +0200

    MyMelvorBlazor 2024-08-01 sweet alert 2 messages

--- MyMelvorBlazor/MyMelvorBlazor/Program.cs
+++ MyMelvorBlazor/MyMelvorBlazor/Program.cs
@@ -1,3 +1,4 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -14,6 +15,8 @@ namespace MyMelvorBlazor
 			builder.RootComponents.Add<HeadOutlet>("head::after");
 
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+			builder.Services.AddSweetAlert2();
 
 			await builder.Build().RunAsync();
 		}
