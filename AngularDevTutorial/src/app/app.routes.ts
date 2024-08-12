dc56c826b375574e8cec0bf6d0706f8a63d46666
Lusky <lusky@home>
2024-08-12 19:11:58 +0200

    AngularDevTutorial 2024-08-12 Fix angular project for routing

--- AngularDevTutorial/src/app/app.routes.ts
+++ AngularDevTutorial/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import {Routes} from '@angular/router';
 
 import {HomeComponent} from './home/home.component';
 import {UserComponent} from './user/user.component';
+import {TestsComponent} from './tests/tests.component';
 
 export const routes: Routes = [
   {
@@ -13,5 +14,10 @@ export const routes: Routes = [
     path: 'user',
     title: 'App User Page',
     component: UserComponent,
+  },
+  {
+    path: 'tests',
+    title: 'App Test Page',
+    component: TestsComponent,
   },
 ];
