df248c50ac4d179126f11d28ec0ddcaa33dab69f
Lusky <lusky@work>
2024-08-13 12:16:32 +0200

    AngularDevTutorial 2024-08-13 service and pipes

--- AngularDevTutorial/src/app/app.component.ts
+++ AngularDevTutorial/src/app/app.component.ts
@@ -10,5 +10,5 @@ import { TestsComponent } from "./tests/tests.component";
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
+  
 }
--- AngularDevTutorial/src/app/user/user.component.ts
+++ AngularDevTutorial/src/app/user/user.component.ts
@@ -1,14 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { FormsModule, ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { CarService } from '../car.service';
+import { LowerCasePipe, DecimalPipe, DatePipe, CurrencyPipe } from '@angular/common';
+import { ReversePipe } from '../reverse.pipe';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, LowerCasePipe, DecimalPipe, DatePipe, CurrencyPipe, ReversePipe],
   template: `
     <p>The occupation user's name is {{occupation}}</p>
-    <p>{{ username }}'s favorite framework: {{ favoriteFramework }}</p>
+    <p>{{ username | lowercase }}'s favorite framework: {{ favoriteFramework }}</p>
     <label for="framework">
       Favorite framework:
       <input id="framework" type="text" [(ngModel)]="favoriteFramework"/>
@@ -33,19 +35,44 @@ import { FormsModule, ReactiveFormsModule, FormControl, FormGroup, Validators } 
     <h2>Profile Form</h2>
     <p>Name: {{ profileForm.value.name }}</p>
     <p>Email: {{ profileForm.value.email }}</p>
+
+    <br>
+    <p>Car Listing: {{ display }}</p>
+
+    <br>
+    <ul>
+      <li>Number with "decimal" {{ num | number:"3.2-2"}}</li>
+      <li>Date with "date" {{ birthday | date:'medium'}}</li>
+      <li>Currency with "currency" {{ cost | currency}}</li>
+      <li>Reverse of lowercase of {{username}} is {{username | lowercase | reverse}}</li>
+    </ul>
   `,
   styles: ``
 })
 export class UserComponent {
   @Input() occupation = 'default';
 
-  username = 'youngtech';
+  username = 'yOUngtEch';
   favoriteFramework = '';
 
   profileForm = new FormGroup({
     name: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
   });
+
+  //syntax with inject
+  //carService = inject(CarService)
+
+  display = '';
+
+  num = 103.1234;
+  birthday = new Date(2023, 3, 2);
+  cost = 4560.34;
+
+  //syntax without inject
+  constructor(private carService: CarService) {
+    this.display = this.carService.getCars().join(' ⭐️ ');
+  }
 
   showFramework() {
     alert(this.favoriteFramework)
