0ab0522963fae94084f00109e293fabf05e3cf5b
Lusky <lusky@work>
2024-08-13 10:46:38 +0200

    AngularDevTutorial 2024-08-13 forms

--- AngularDevTutorial/src/app/user/user.component.ts
+++ AngularDevTutorial/src/app/user/user.component.ts
@@ -1,14 +1,57 @@
 import { Component, Input } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   template: `
-   <p>The user's name is {{occupation}}</p>
+    <p>The occupation user's name is {{occupation}}</p>
+    <p>{{ username }}'s favorite framework: {{ favoriteFramework }}</p>
+    <label for="framework">
+      Favorite framework:
+      <input id="framework" type="text" [(ngModel)]="favoriteFramework"/>
+    </label>
+    <button (click)="showFramework()">Show framework</button>
+    <br>
+    <br>
+    <form [formGroup]="profileForm" (ngSubmit)="handleSubmit()">
+      <label>
+        Name
+        <input type="text" formControlName="name" />
+      </label>
+      <br>
+      <label>
+        Email
+        <input type="email" formControlName="email" />
+      </label>
+      <button type="submit" [disabled]="!profileForm.valid">Submit</button>
+    </form>
+
+    <br>
+    <h2>Profile Form</h2>
+    <p>Name: {{ profileForm.value.name }}</p>
+    <p>Email: {{ profileForm.value.email }}</p>
   `,
   styles: ``
 })
 export class UserComponent {
-  @Input() occupation = 'init';
+  @Input() occupation = 'default';
+
+  username = 'youngtech';
+  favoriteFramework = '';
+
+  profileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+
+  showFramework() {
+    alert(this.favoriteFramework)
+  }
+
+  handleSubmit() {
+    alert(this.profileForm.value.name + ' | ' + this.profileForm.value.email);
+  }
 }
