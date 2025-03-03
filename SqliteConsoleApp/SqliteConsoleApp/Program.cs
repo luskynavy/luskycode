4b08d24bc2ff05c1a3f648662bd075177eecd6e1
Lusky <lusky@home>
2025-03-03 11:56:35 +0100

    SqliteConsoleApp 2025-03-03 Test with transactions

--- SqliteConsoleApp/SqliteConsoleApp/Program.cs
+++ SqliteConsoleApp/SqliteConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Data.Sqlite;
+using System.Data.Common;
 using System.Diagnostics;
+using System.Transactions;
 using System.Xml.Linq;
 
 namespace SqliteConsoleApp
@@ -32,19 +34,28 @@ namespace SqliteConsoleApp
                 Console.WriteLine($"Elements in tabletest {nb}");
 
                 int maxInsert = 10000;
-                for (int i = 0; i < maxInsert; i++)
-                {
-                    command.CommandText = "INSERT INTO tabletest (id, col1, col2) VALUES (" + i + ", 2, 3)";
-                    command.ExecuteNonQuery();
 
-                    if (i % 200 == 0)
+                // Transaction for fast speed batch inserts
+                using (var transaction = db.BeginTransaction())
+                {
+                    command.Transaction = transaction;
+                    for (int i = 0; i < maxInsert; i++)
                     {
-                        System.Console.WriteLine((double)i + " in " + (double)sw.Elapsed.TotalMilliseconds / 1000 + " : " + (double)i / sw.Elapsed.TotalMilliseconds * 1000);
+                        command.CommandText = "INSERT INTO tabletest (id, col1, col2) VALUES (" + i + ", 2, 3)";
+                        command.ExecuteNonQuery();
+
+                        if (i % 200 == 0)
+                        {
+                            System.Console.WriteLine((double)i + " in " + (double)sw.Elapsed.TotalMilliseconds / 1000 + " : " + (double)i / sw.Elapsed.TotalMilliseconds * 1000);
+                        }
                     }
+
+                    transaction.Commit();
                 }
 
                 System.Console.WriteLine((double)maxInsert + " in " + (double)sw.Elapsed.TotalMilliseconds / 1000 + " : " + (double)maxInsert / sw.Elapsed.TotalMilliseconds * 1000);
 
+                command.Transaction = null;
                 command.CommandText = @"SELECT COUNT(*) as nb FROM tabletest";
                 nb = command.ExecuteScalar();
                 Console.WriteLine($"Elements in tabletest {nb}");
