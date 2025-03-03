4afe1dc17549f8e6a5c33b1c386331b0f4297a83
Lusky <lusky@home>
2025-03-03 12:16:31 +0100

    NodeJs/nodejs-api-masterclass/nodehttp 2025-03-03 GET & POST todos

--- NodeJs/nodejs-api-masterclass/nodehttp/server.js
+++ NodeJs/nodejs-api-masterclass/nodehttp/server.js
@@ -1,34 +1,55 @@
-const http = require ('http');
+const http = require("http");
 
 const todos = [
-    {id: 1, text: 'Todo one'},
-    {id: 2, text: 'Todo two'},
-    {id: 3, text: 'Todo three'},
+  { id: 1, text: "Todo one" },
+  { id: 2, text: "Todo two" },
+  { id: 3, text: "Todo three" },
 ];
 
 const server = http.createServer((req, res) => {
-    const { headers, url, method } = req ;
-    console.log(headers, url, method);
+  const { url, method } = req;
+  console.log(url, method);
 
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'X-Powered-By': 'Node.js'
-        }
-    );
+  let body = [];
 
-    let body = [];
-
-    req.on('data', chunk => {
-        body.push(chunk);
-    }).on('end', () => {
-        body = Buffer.concat(body).toString();
-        console.log(body);
+  req
+    .on("data", (chunk) => {
+      body.push(chunk);
     })
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
 
-    res.end(JSON.stringify({
-        success: true,
-        data: todos
-    }));
+      let status = 404;
+      const response = {
+        success: false,
+        data: null,
+        error: null
+      };
+
+      if (method === "GET" && url === "/todos") {
+        status = 200;
+        response.success = true;
+        response.data = todos;
+      } else if (method === "POST" && url === "/todos") {
+        const { id, text } = JSON.parse(body);
+
+        if (!id || !text) {
+          status = 400;
+          response.error = "Please add id and text"
+        } else {
+          todos.push({ id, text });
+          status = 201;
+          response.success = true;
+          response.data = todos;
+        }
+      }
+
+      res.writeHead(status, {
+        "Content-Type": "application/json",
+        "X-Powered-By": "Node.js",
+      });
+      res.end(JSON.stringify(response));
+    });
 });
 
 const PORT = 5000;
