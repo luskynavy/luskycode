80b971f07ad8a48885cdca7c6787d823be7d9831
Lusky <lusky@home>
2025-03-04 15:41:07 +0100

    NodeJs/nodejs-api-masterclass/devcamper_api 2025-03-03 MongoDB config with mongoose

--- NodeJs/nodejs-api-masterclass/devcamper_api/server.js
+++ NodeJs/nodejs-api-masterclass/devcamper_api/server.js
@@ -1,33 +1,38 @@
 const express = require('express');
 const dotenv = require('dotenv');
+//const logger = require('./middleware/logger');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({path: './config/config.env'});
 
+// Connect to database
+
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
+
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({ success: true, msg: "Show all bootcamps"});
-});
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+//app.use(logger);
 
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}`});
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({ success: true, msg: "Create new bootcamp"});
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}`});
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}`});
-});
-
-
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
