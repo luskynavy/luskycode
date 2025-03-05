3a6fa26fae03691f1683738f3265822608eb5c01
Lusky <lusky@home>
2025-03-05 20:02:47 +0100

    NodeJs/nodejs-api-masterclass/devcamper_api 2025-03-05 Error handler, async handler, fake geocoder

--- NodeJs/nodejs-api-masterclass/devcamper_api/server.js
+++ NodeJs/nodejs-api-masterclass/devcamper_api/server.js
@@ -3,18 +3,17 @@ const dotenv = require('dotenv');
 //const logger = require('./middleware/logger');
 const morgan = require('morgan');
 const colors = require('colors');
+const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({path: './config/config.env'});
 
 // Connect to database
-
 connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
-
 
 const app = express();
 
@@ -27,7 +26,10 @@ if (process.env.NODE_ENV === 'development') {
 }
 //app.use(logger);
 
+// Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
