b76a9a12de3b0a0fd25017abd3696fe326f1a08c
Lusky <lusky@home>
2025-04-02 01:34:12 +0200

    ReceiptsQt 2025-04-02 Fill group list with db

--- ReceiptsQt/mainwindow.cpp
+++ ReceiptsQt/mainwindow.cpp
@@ -15,9 +15,80 @@ MainWindow::MainWindow(QWidget *parent)
     ui->sortComboBox->addItem("Groupe", "group");
     ui->sortComboBox->addItem("Date de réception", "dateReceipt");
     ui->sortComboBox->addItem("Nom", "name");
+
+    initDb();
+
+    getGroupList();
+
+    _ready = true;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_searchButton_clicked()
+{
+    submit();
+}
+
+
+void MainWindow::on_clearButton_clicked()
+{
+    _ready = false;
+    ui->groupComboBox->setCurrentIndex(0);
+    ui->nameLineEdit->setText("");
+    ui->sortComboBox->setCurrentIndex(0);
+    //submit();
+    _ready = true;
+}
+
+void MainWindow::on_pagesizeComboBox_currentIndexChanged(int index)
+{
+    if (_ready)
+    {
+        auto val = ui->pagesizeComboBox->itemData(index);
+        _currentPageSize = val.toInt();
+        submit();
+    }
+}
+
+void MainWindow::initDb()
+{
+    _db = QSqlDatabase::addDatabase("QMYSQL");
+    _db.setDatabaseName(_databaseName);
+    _db.setHostName(_host);
+    _db.setPort(_port);
+    if (!_db.open(_user, _password)) {
+
+    }
+}
+
+void MainWindow::getGroupList()
+{
+
+    QSqlQuery query(_db);
+    query.exec("SELECT DISTINCT `group` FROM products ORDER BY `group`");
+    ui->groupComboBox->addItem("");
+    while (query.next()) {
+        QString group = query.value(0).toString();
+        ui->groupComboBox->addItem(group);
+    }
+}
+
+void MainWindow::submit()
+{
+    _currentGroup = ui->groupComboBox->itemText(ui->groupComboBox->currentIndex());
+
+    _currentName = ui->nameLineEdit->text();
+
+    auto val = ui->sortComboBox->itemData(ui->sortComboBox->currentIndex());
+    _currentSort = val.toString();
+
+    qDebug() << "group " << _currentGroup;
+    qDebug() << "name " << _currentName;
+    qDebug() << "sort " << _currentSort;
+    qDebug() << "page " << _currentPage;
+    qDebug() << "pagesize " << _currentPageSize;
 }
--- ReceiptsQt/mainwindow.h
+++ ReceiptsQt/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtSql>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +18,33 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_searchButton_clicked();
+
+    void on_clearButton_clicked();
+
+    void on_pagesizeComboBox_currentIndexChanged(int index);
+
 private:
     Ui::MainWindow *ui;
+    bool _ready = false;
+    QString _currentSort = "";
+    QString _currentGroup = "";
+    QString _currentName = "";
+    int _currentPage = 1;
+    int _currentPageSize = 10;
+    int _totalPages = 1;
+
+    QSqlDatabase _db;
+
+    QString _host = "localhost";
+    int _port = 3306;
+    QString _user = "root";
+    QString _password = "";
+    QString _databaseName = "receipts";
+
+    void initDb();
+    void submit();
+    void getGroupList();
 };
 #endif // MAINWINDOW_H
