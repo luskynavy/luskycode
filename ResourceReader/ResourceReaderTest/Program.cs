17935212acf0e2d5759d68670623a2b0600b8241
Lusky <lusky@home>
2025-10-28 23:38:21 +0100

    ResourceReaderTest 2025-10-28 ResourceReader and DeserializingResourceReader test

---
+++ ResourceReader/ResourceReaderTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Resources;
+using System.Resources.Extensions;
+
+namespace ResourceReaderTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> resourcesFiles = [
+                @".\ResourceReaderTest.dll",
+                @".\en\ResourceReaderTest.resources.dll",
+                @".\fr\ResourceReaderTest.resources.dll"
+            ];
+
+            foreach (string file in resourcesFiles)
+            {
+                Console.WriteLine(file + ":");
+                var assem = System.Reflection.Assembly.LoadFrom(file);
+                foreach (var resourceName in assem.GetManifestResourceNames())
+                {
+                    Console.WriteLine(resourceName + ":");
+                    var stream = assem.GetManifestResourceStream(resourceName);
+
+                    //ok
+                    //var reader = new System.Resources.Extensions.DeserializingResourceReader(stream);
+
+                    //ok too and don't need System.Resources.Extensions.dll
+                    var reader = new ResourceReader(stream);
+                    IDictionaryEnumerator dict = reader.GetEnumerator();
+                    int ctr = 0;
+
+                    while (dict.MoveNext())
+                    {
+                        ctr++;
+                        Console.WriteLine("{0:00}: {1} = {2}", ctr, dict.Key, dict.Value);
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
