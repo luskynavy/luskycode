9ede41c8c1d315e59413c68ba46ab318f8de219b
Lusky <lusky@home>
2025-12-16 00:49:20 +0100

    ToastCSharp 2025-12-16 Windows Toasts/Notifications test in to open url

---
+++ Toast/ToastCSharp/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ToastCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ButtonSendToast = new Button();
+            SuspendLayout();
+            // 
+            // ButtonSendToast
+            // 
+            ButtonSendToast.Location = new Point(277, 121);
+            ButtonSendToast.Name = "ButtonSendToast";
+            ButtonSendToast.Size = new Size(94, 29);
+            ButtonSendToast.TabIndex = 0;
+            ButtonSendToast.Text = "Send Toast";
+            ButtonSendToast.UseVisualStyleBackColor = true;
+            ButtonSendToast.Click += ButtonSendToast_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ButtonSendToast);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button ButtonSendToast;
+    }
+}
---
+++ Toast/ToastCSharp/Form1.cs
@@ -0,0 +1,70 @@
+using Microsoft.Toolkit.Uwp.Notifications;
+using System.Diagnostics;
+using System.Security.Policy;
+using Windows.Foundation.Collections;
+
+namespace ToastCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Listen to notification activation
+            ToastNotificationManagerCompat.OnActivated += OnToastActivated;
+        }
+
+        private void OnToastActivated(ToastNotificationActivatedEventArgsCompat toastArgs)
+        {
+            ToastArguments args = ToastArguments.Parse(toastArgs.Argument);
+
+            if (args.Contains("conversationId"))
+            {
+                // Show the corresponding content
+                //MessageBox.Show("Toast activated. Args: " + toastArgs.Argument);
+
+                // Fail in .net core, ok for .Net ?
+                // System.Diagnostics.Process.Start("https://google.com");
+
+                // Fail too in .net core
+                /*ProcessStartInfo psInfo = new ProcessStartInfo
+                {
+                    FileName = "https://www.google.com",
+                    UseShellExecute = true
+                };*/
+
+                // .Net core version
+                Process.Start("explorer", "https://www.google.com");
+            }
+        }
+
+
+        private void ButtonSendToast_Click(object sender, EventArgs e)
+        {
+            // Create toast with one minute expiration
+            new ToastContentBuilder()
+                .AddArgument("action", "viewConversation")
+                .AddArgument("conversationId", 9813)
+                .AddText("ToastC# update available")
+                .AddText("Version 2026 is now avaliable")
+                .Show(toast =>
+                {
+                    toast.ExpirationTime = DateTime.Now.AddMinutes(1);
+                }); // Not seeing the Show() method? Make sure you have version 7.0,
+                         // and if you're using .NET 6 (or later),
+                         // then your TFM must be net6.0-windows10.0.17763.0 or greater
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Clear toast list on leaving
+            ToastNotificationManagerCompat.Uninstall();
+        }
+    }
+}
